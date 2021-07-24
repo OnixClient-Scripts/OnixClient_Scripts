c8552e8f4756b8fa5de1335a5311740c019c5a17
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-07-25 00:01:26 +0900

    Update arrowcounter.lua

--- Modules/arrowcounter.lua
+++ Modules/arrowcounter.lua
@@ -12,6 +12,10 @@ positionX = 90
 positionY = 20
 size = 1
 imagePath = "arrow.png"
+bowOnly = true   --crossbows too, will be invisible if you don't hold one
+
+bowId = 300
+crossbowId = 575
 
 function update(deltaTime)
     
@@ -20,21 +24,25 @@ end
 
 function render(deltaTime)
     local inventory = player.inventory()
-    local arrowCount = 0
-    for i=1,inventory.size do
-        local slot = inventory.at(i)
-        if (slot ~= nil and slot.id == 301) then
-            arrowCount = arrowCount + slot.count
+    local potCount = 0
+    local selected = inventory.at(inventory.selected)
+    if (bowOnly == false or (selected ~= nil and (selected.id == bowId or selected.id == crossbowId))) then
+        for i=1,inventory.size do
+            local slot = inventory.at(i)
+            if (slot ~= nil and slot.id == 301) then
+                potCount = potCount + slot.count
+            end
         end
-    end
-    local font = gui.font()
-    local text = " Arrow Count: " .. arrowCount
-
-    gfx.color(0,0,0,120)
-    gfx.rect(positionX, positionY, size*1.2*font.width(text), size*10)
-
-    gfx.color(255, 255, 255)
     
-    gfx.text(positionX+10, positionY + 2, text, size)
-    gfx.image(positionX, positionY, size*10, size*10, imagePath)
+        local font = gui.font()
+        local text = " Arrow Count: " .. potCount
+
+        gfx.color(0,0,0,120)
+        gfx.rect(positionX, positionY, size*1.2*font.width(text), size*10)
+
+        gfx.color(255, 255, 255)
+        
+        gfx.text(positionX+10, positionY + 2, text, size)
+        gfx.image(positionX, positionY, size*10, size*10, imagePath)
+    end
 end
