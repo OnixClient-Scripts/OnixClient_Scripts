602ba83e1637a71f1cef78f0be0a79d0dedfbebc
MCBE Craft <mcbe.craft0@gmail.com>
2021-07-27 08:09:34 +0200

    added world version
    tells the version of minecraft you are using

--- Commands/worldinfo.lua
+++ Commands/worldinfo.lua
@@ -6,8 +6,10 @@ function execute(arguments)
 	local dimensionName = dimension.name()
 	local time = dimension.time()
 	local worldName = server.worldName()
+	local worldVersion = game.version
 	
 	print("World Name: " .. worldName)
+	print("World Version: " .. worldVersion)
 	print("Dimension: " .. dimensionName .. " (" .. dimensionId .. ")")
 	print("Dimension Time: " .. time)
 end
