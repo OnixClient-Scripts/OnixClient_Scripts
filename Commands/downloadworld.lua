fbac129c931fde8ad09ccce7de649944f296a27b
HugoNotHere <80633305+HugoNotHere@users.noreply.github.com>
2021-07-28 13:49:36 +0100

    Add files via upload
    This is kinda unstable
    do .download
    then do  .function import.mcfunction in a void
    by onix

---
+++ Commands/downloadworld.lua
@@ -0,0 +1,33 @@
+command = "download"
+help_message = "download world WARNING, GAME WILL FREEZE OR CRASH DURING DOWNLOAD OR IMPORT"
+
+function execute(arguments)
+
+
+
+    local file = io.open("import.mcfunction", "w")
+    io.output(file)
+
+    local x,y,z = player.position()
+    x = x - 512
+    y = y - 512
+    z = z - 512
+    local ex = x + 1024
+    local ey = y + 1024
+    local ez = z + 1024
+
+    io.write("/fill " .. x .. " " .. y .. " " .. z .. " " .. ex .. " " .. ey .. " " .. ez .. " air 0 replace\n")
+
+    for px=x,ey do
+        for py=x,ey do
+            for pz=x,ey do
+                local block = dimension.getBlock(px,py,pz)
+                if (block.id ~= 0) then
+                    io.write("/setblock " .. px .. " " .. py .. " " .. pz .. " " .. block.name .. " " .. block.data .. "\n")
+                end
+            end
+        end
+    end
+    io.close(file)
+    print ("§aSuccessfully downloaded world!")
+end
