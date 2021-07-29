7d33095e1614e5fab1a61ee4eab151db6dfbc456
MCBE Craft <mcbe.craft0@gmail.com>
2021-07-29 09:00:37 +0200

    optimized for better fps
    doesnt draw the empty hunger all the time anymore

--- Modules/saturationdisplay.lua
+++ Modules/saturationdisplay.lua
@@ -21,15 +21,15 @@ end
 function render(deltaTime)
     local attributeList = player.attributes()
     local saturation = math.floor(attributeList.name("minecraft:player.saturation").value)
-    gfx.color(255, 0, 0, 0)
     for i = 0, 9 do
-        gfx.image((positionX) - (i * 8), positionY - 3, 9, 9, imagePathEmpty)
-    end
-    for i=0,(saturation - 1)/2 do
-        if (i ~= (saturation - 1)/2) then
-            gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathFull)
+        if (saturation/2 > i) then
+            if (i ~= (saturation - 1)/2) then
+                gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathFull)
+            else
+                gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathHalf)
+            end
         else
-            gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathHalf)
+            gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathEmpty)
         end
     end
 end
