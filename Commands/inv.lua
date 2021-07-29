295f35060378ecbc91d6f03df9a86d766843b338
MCBE Craft <hug.dp0@gmail.com>
2021-07-29 14:24:59 +0200

    get inventory or special items from it and the amount

---
+++ Commands/inv.lua
@@ -0,0 +1,55 @@
+command = "inv"
+help_message = "sends current inventory"
+
+function execute(arguments)
+    if (arguments == "") then
+        local inventory = player.inventory()
+        local armor = inventory.armor()
+        if (armor.helmet ~= nil) then
+            print("1 " .. armor.helmet.name .. " " .. armor.helmet.data .. " " .. armor.helmet.id)
+        else
+            print("0 helmet 0")
+        end
+        if (armor.chestplate ~= nil) then
+            print("1 " .. armor.chestplate.name .. " " .. armor.chestplate.data .. " " .. armor.chestplate.id)
+        else
+            print("0 chestplate 0")
+        end
+        if (armor.leggings ~= nil) then
+            print("1 " .. armor.leggings.name .. " " .. armor.leggings.data .. " " .. armor.leggings.id)
+        else
+            print("0 leggings 0")
+        end
+        if (armor.boots ~= nil) then
+            print("1 " .. armor.boots.name .. " " .. armor.boots.data .. " " .. armor.boots.id)
+        else
+            print("0 boots 0")
+        end
+        if (inventory.offhand() ~= nil) then
+            print(inventory.offhand().count .. " " .. inventory.offhand().name .. " " .. inventory.offhand().maxData - inventory.offhand().data .. " " .. inventory.offhand().id)
+        end
+        for i=1,inventory.size do
+            local slot = inventory.at(i)
+            if (slot ~= nil) then
+                print(slot.count .. " " .. slot.name .. " " .. slot.maxData - slot.data .. " " .. slot.id)
+            end
+        end
+    else
+        local total = 0
+        local inventory = player.inventory()
+        for i=1,inventory.size do
+            local slot = inventory.at(i)
+            if (slot ~= nil) then
+                if (slot.name == arguments) then
+                    total = total + slot.count
+                end
+            end
+        end
+        if (inventory.offhand() ~= nil) then
+            if (inventory.offhand().name == arguments) then
+                total = total + inventory.offhand().count
+            end
+        end
+        print(arguments .. ": " .. total)
+    end
+end
