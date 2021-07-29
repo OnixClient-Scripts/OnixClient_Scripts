1d4ef7d86d16f398977e9cc32362e8f664211d07
MCBE Craft <mcbe.craft0@gmail.com>
2021-07-29 21:08:24 +0200

    better fps and no need for a arrow.png anymore

--- Modules/arrowcounter.lua
+++ Modules/arrowcounter.lua
@@ -1,16 +1,8 @@
 name = "Arrow Counter"
 description = "gives the amout of arrow"
 
---[[
-    Arrow Counter Module Script
-	
-	made by MCBE Craft
-	improvement by Onix86
-]]
-
-positionX = 90
-positionY = 20
-size = 1
+positionX = 5
+positionY = 210
 imagePath = "arrow.png"
 bowOnly = true   --crossbows too, will be invisible if you don't hold one
 
@@ -18,7 +10,7 @@ bowId = 300
 crossbowId = 575
 
 function update(deltaTime)
-    
+
 end
 
 
@@ -26,26 +18,31 @@ function render(deltaTime)
     local inventory = player.inventory()
     local arrowCount = 0
     local selected = inventory.at(inventory.selected)
+    local itemLocation = ""
     if (bowOnly == false or (selected ~= nil and (selected.id == bowId or selected.id == crossbowId))) then
         for i=1,inventory.size do
             local slot = inventory.at(i)
             if (slot ~= nil and slot.id == 301) then
                 arrowCount = arrowCount + slot.count
+                itemLocation = slot.location
             end
         end
-	if (inventory.offhand() ~= nil and inventory.offhand().id == 301) then
+        if (inventory.offhand() ~= nil and inventory.offhand().id == 301) then
             arrowCount = arrowCount + inventory.offhand().count
+            itemLocation = inventory.offhand().location
         end
-    
-        local font = gui.font()
-        local text = " Arrow Count: " .. arrowCount
+        if (itemLocation ~= "") then
+            local font = gui.font()
+            local text = " Arrow Count: " .. arrowCount
 
-        gfx.color(0,0,0,120)
-        gfx.rect(positionX, positionY, size*1.2*font.width(text), size*10)
+            gfx.color(0,0,0,0)
+            gfx.rect(positionX, positionY, 12 + font.width(text), 10)
 
-        gfx.color(255, 255, 255)
+            gfx.color(255, 0, 0)
+
+            gfx.text(positionX + 17, positionY + 3, text, 1)
+            gfx.item(positionX, positionY - 3, itemLocation)
+        end
         
-        gfx.text(positionX+10, positionY + 2, text, size)
-        gfx.image(positionX, positionY, size*10, size*10, imagePath)
     end
 end
