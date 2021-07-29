b9bdfa9f0d140af3e93a475b427fe248c409a29e
MCBE Craft <mcbe.craft0@gmail.com>
2021-07-29 21:07:18 +0200

    hides it in creative

--- Modules/saturationdisplay.lua
+++ Modules/saturationdisplay.lua
@@ -1,12 +1,6 @@
 name = "Saturation display"
 description = "gives the amout of saturation"
 
---[[
-    Saturation Display Module Script
-	
-	made by MCBE Craft
-]]
-
 positionX = 401
 positionY = 285
 imagePathFull = "hunger_full.png"
@@ -19,17 +13,19 @@ end
 
 
 function render(deltaTime)
-    local attributeList = player.attributes()
-    local saturation = math.floor(attributeList.name("minecraft:player.saturation").value)
-    for i = 0, 9 do
-        if (saturation/2 > i) then
-            if (i ~= (saturation - 1)/2) then
-                gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathFull)
+    if (player.gamemode() ~= 1) then
+        local attributeList = player.attributes()
+        local saturation = math.floor(attributeList.name("minecraft:player.saturation").value)
+        for i = 0, 9 do
+            if (saturation/2 > i) then
+                if (i ~= (saturation - 1)/2) then
+                    gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathFull)
+                else
+                    gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathHalf)
+                end
             else
-                gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathHalf)
+                gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathEmpty)
             end
-        else
-            gfx.image(positionX - (i * 8), positionY - 3, 9, 9, imagePathEmpty)
         end
     end
 end
