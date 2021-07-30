c1aacee83c466fa5b922fbcb42bd6f5066fb473d
MCBE Craft <mcbe.craft0@gmail.com>
2021-07-30 10:49:12 +0200

    doubled the fps

--- Modules/minimap.lua
+++ Modules/minimap.lua
@@ -3,8 +3,8 @@ description = "shows a map of blocks around you"
 
 --[[
     Minimap Module Script
-	
-	made by MCBE Craft
+    
+    made by MCBE Craft
 ]]
 
 positionX = 575
@@ -13,32 +13,42 @@ size = 10
 opacity = 3
 
 function update(deltaTime)
+    x,y,z = player.position()
+    lx,ly,lz = player.lookingPos()
+    sx = x - size
+    sy = y - math.floor(size/2)
+    sz = z - size
+    ex = sx + (2 * size)
+    ey = y + (size)
+    ez = sz + (2 * size)
+    array = {}
+    for i=sx,ex do
+        array[i] = {}
+    end
+
+    for px=sx,ex do
+        for pz=sz,ez do
+            local highest = sy - 1
+            for py=sy,ey do
+                local block = dimension.getBlock(px,ey - (py - sy),pz)
+                if (block.id ~= 0) then
+                    highest = ey - (py - sy)
+                    goto next_block
+                end
+            end
+            ::next_block::
+            array[px][pz] = highest
+        end
+    end
 
 end
 
 
 function render(deltaTime)
-    local x,y,z = player.position()
-    local lx,ly,lz = player.lookingPos()
-    local sx = x - size
-    local sy = y - math.floor(size/2)
-    local sz = z - size
-    local ex = sx + (2 * size)
-    local ey = y + (size)
-    local ez = sz + (2 * size)
-
-
     gfx.color(255,255,255, math.floor(255 / size * opacity))
     for px=sx,ex do
         for pz=sz,ez do
-            local highest = sy - 1
-            for py=sy,ey do
-                local block = dimension.getBlock(px,py,pz)
-                if (block.id ~= 0) then
-                    highest = py
-                end
-            end
-            for i=sy,highest do
+            for i=sy,array[px][pz]do
                 gfx.rect(positionX + (4 * (px - x)), positionY + (4 * (pz - z)), 4, 4)
             end
         end
