9a2fa8e3ab2a161e0e72e75144c241bfaeab546d
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-08-09 01:27:24 +0900

    Add files via upload

---
+++ Commands/notification_example.lua
@@ -0,0 +1,8 @@
+command = "test"
+help_message = "testing command"
+
+
+function execute(arguments)
+    print("cool notif shouldve happened")
+    client.notification("aaaaaaaaaaaaaaaaaaaaaaaaa")
+en
