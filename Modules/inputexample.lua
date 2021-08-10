68b6fcb92e3ac56dd028f7c3f70e27cc8a7506f6
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-08-10 22:40:00 +0900

    Update inputexample.lua

--- Modules/inputexample.lua
+++ Modules/inputexample.lua
@@ -4,6 +4,9 @@ description = "Keyboard, Mouse input example module script"
 --[[
     Keyboard, Mouse input example module script
     
+    you can find key value here
+    https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+    
     made by Quoty0
 ]]
 
