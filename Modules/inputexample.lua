7c3aff6a4fb2e9221c6f69fb619973aa111a4090
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-08-10 22:10:41 +0900

    Add files via upload

---
+++ Modules/inputexample.lua
@@ -0,0 +1,25 @@
+name = "Input Example"
+description = "Keyboard, Mouse input example module script"
+
+--[[
+    Keyboard, Mouse input example module script
+    
+    made by Quoty0
+]]
+
+function keyboard(key, isDown)
+	print(key)
+end
+
+function mouse(button, isDown)
+	print(button)
+end
+
+function update(deltaTime)
+    
+end
+
+
+function render(deltaTime)
+
+end
