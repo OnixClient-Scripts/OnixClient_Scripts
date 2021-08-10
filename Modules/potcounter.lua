ea73f7cd8d59d4f38444a8b1883181f1386313d9
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2021-08-10 10:04:14 -0400

    improvements, options, update to 1.17.11

--- Modules/potcounter.lua
+++ Modules/potcounter.lua
@@ -7,13 +7,26 @@ description = "gives the amout of healing pots"
 	made by MCBE Craft
 	improvement by Onix86
 ]]
-
 positionX = 90
 positionY = 10
-size = 1
+sizeX = 30
+sizeY = 10
+
+Background_Color_R = 0
+Background_Color_G = 0
+Background_Color_B = 0
+Background_Color_A = 127
+
+Text_Color_R = 255
+Text_Color_G = 0
+Text_Color_B = 0
+Text_Color_A = 255
+
+potionId = 561
+texturePath = "textures/items/potion_bottle_splash_heal"
 
 function update(deltaTime)
-    
+
 end
 
 
@@ -22,18 +35,25 @@ function render(deltaTime)
     local potCount = 0
     for i=1,inventory.size do
         local slot = inventory.at(i)
-        if (slot ~= nil and slot.id == 551) then
+        if (slot ~= nil and slot.id == potionId) then
             potCount = potCount + slot.count
-        end
+            itemLocation = slot.location
+         end
     end
+
+    local offhand = inventory.offhand()
+    if (offhand ~= nil and offhand.id == potionId) then
+        potCount = potCount + offhand.count
+    end
+        
     local font = gui.font()
     local text = " Pot Count: " .. potCount
 
-    gfx.color(0,0,0,120)
-    gfx.rect(positionX, positionY, size*1.3*font.width(text), size*10)
+    gfx.color(Background_Color_R,Background_Color_G,Background_Color_B,Background_Color_A)
+    sizeX = 14 + font.width(text)
+    gfx.rect(0, 0, sizeX, 10)
 
-    gfx.color(255, 255, 255)
-    
-    gfx.text(positionX+10, positionY + 2, text, size)
-    gfx.texture(positionX, positionY, size*10, size*10, "textures/items/potion_bottle_splash_heal.png", 100)
+    gfx.color(Text_Color_R, Text_Color_G, Text_Color_B, Text_Color_A)
+    gfx.text(12, 5 - (font.height / 2), text, 1)
+    gfx.texture(0, 0, 10, 10, texturePath, Text_Color_A)
 end
