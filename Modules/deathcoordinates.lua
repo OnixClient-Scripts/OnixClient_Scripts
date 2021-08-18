240595c34585e278c2b20fb2ea70bd04909a34bf
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-08-18 21:06:22 +0900

    Update deathcoordinates.lua

--- Modules/deathcoordinates.lua
+++ Modules/deathcoordinates.lua
@@ -18,16 +18,16 @@ end
 
 function render()
     local player_x,player_y,player_z = player.position()
-	local attribs = player.attributes()
-	local health = attribs.id(7).value
+    local attribs = player.attributes()
+    local health = attribs.id(7).value
 	
-	if health > 0 then
-	    check = true
-	end
+    if health > 0 then
+        check = true
+    end
 	
-	if health == 0 and check == true then
-	    print("§cYou died! Your §4death coordinates §care:")
-	    print("§8(§7" .. player_x .. " " .. player_y .. " " .. player_z .. "§8)")
-            check = false
+    if health == 0 and check == true then
+    print("§cYou died! Your §4death coordinates §care:")
+    print("§8(§7" .. player_x .. " " .. player_y .. " " .. player_z .. "§8)")
+    check = false
     end
 end
