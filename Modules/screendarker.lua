c6c7a66c98c1380a4e264c09bb598f7bed4ce0ae
HugoNotHere <80633305+HugoNotHere@users.noreply.github.com>
2021-08-23 09:33:38 +0100

    Create screendarker.lua
    please don't ask me why this is a thing as there is legit an env changer ;-;

---
+++ Modules/screendarker.lua
@@ -0,0 +1,13 @@
+name = "screendarker"
+description = "A simple module to test lua module stuff"
+
+
+function render(deltaTime)
+
+
+    
+    
+    gfx.color(0,0,0,120)
+    gfx.rect(0, 0, 1000, 1000)
+
+end
