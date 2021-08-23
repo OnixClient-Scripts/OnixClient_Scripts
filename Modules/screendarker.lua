5d3083cc9f499d933e0762873a433b44f985a6b1
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-08-23 17:46:51 +0900

    added description

--- Modules/screendarker.lua
+++ Modules/screendarker.lua
@@ -1,12 +1,13 @@
 name = "screendarker"
-description = "A simple module to test lua module stuff"
+description = "Make screen darker (make a black dimm on your screen)"
 
+[[--
+    ScreenDarker Module Script
+
+    made by hugo
+]]
 
 function render(deltaTime)
-
-
-    
-    
     gfx.color(0,0,0,120)
     gfx.rect(0, 0, 1000, 1000)
 
