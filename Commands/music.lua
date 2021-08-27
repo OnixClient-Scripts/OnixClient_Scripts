2e706c7b9e3fdabc43ef4eb37bc7e474e603212b
MCBE Craft <mcbe.craft0@gmail.com>
2021-08-27 13:37:03 +0200

    added possible music

--- Commands/music.lua
+++ Commands/music.lua
@@ -1,6 +1,9 @@
 command = "music"
 help_message = "plays music"
 
+--you can put in
+--11, 13, blocks, chirp, far, mall, mellohi, pigstep, stal, strad, wait, ward
+
 function execute(arguments)
     gui.sound("record." .. arguments)
     print("Now playing: " .. arguments)
