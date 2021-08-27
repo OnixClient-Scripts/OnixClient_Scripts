d0d660e6da3a83ec82e7039e0b0ebd08e291ff05
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-08-27 18:41:17 +0900

    Update offhanddisplay.lua

--- Modules/offhanddisplay.lua
+++ Modules/offhanddisplay.lua
@@ -1,15 +1,17 @@
 name = "Offhand slot display"
 description = "shows the offhand item"
 
-hide = false
-
-positionX = 366
-positionY = 487
+positionX = 200
+positionY = 310
 sizeX = 22
 sizeY = 22
 
 path = "textures/ui/hotbar_0"
-opacity = 1
+opacity = 0.75
+
+--[[
+    Original module made by MCBE Craft
+]]--
 
 function update(deltaTime)
 
@@ -17,22 +19,23 @@ end
 
 
 function render(deltaTime)
-    if(hide == false) then
-        if (gui.mouseGrabbed() == false) then
-            local inventory = player.inventory()
-            local offhandItem = inventory.offhand()
-            if (offhandItem ~= nil) then
-                gfx.texture(0, 0, sizeX, sizeY, path, opacity)
-                gfx.item(369, 490, offhandItem.location, 1)
-                if (offhandItem.count ~= 0 and player.gamemode() ~= 1) then
-                    gfx.color(255,255,255, math.floor(255 * opacity))
-                    if (offhandItem.count >= 10) then
-                        gfx.text(8, 11, offhandItem.count, 1)
-                    else
-                        gfx.text(14, 11, offhandItem.count, 1)
-                    end
+
+    if (gui.mouseGrabbed() == false then
+        local inventory = player.inventory()
+        local offhandItem = inventory.offhand()
+        if (offhandItem ~= nil) then
+            gfx.texture(0, 0, sizeX, sizeY, path, opacity)
+            gfx.item(3, 3, offhandItem.location, 1)
+            if (offhandItem.count ~= 0 and player.gamemode() ~= 1) then
+                gfx.color(255,255,255, math.floor(255 * opacity))
+                if (offhandItem.count >= 10) then
+                    gfx.text(8, 11, offhandItem.count, 1)
+                else
+                    gfx.text(14, 11, offhandItem.count, 1)
                 end
             end
+        
         end
+        
     end
 end
