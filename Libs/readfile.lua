6c13ec1c4eac6d69aecdff16113e4578b17a9bea
MCBE Craft <mcbe.craft0@gmail.com>
2021-09-12 18:29:10 +0200

    writefile

--- Libs/readfile.lua
+++ Libs/readfile.lua
@@ -1,11 +1,16 @@
---library used to read color file, check .color command
-
-function readFile(path)
-    local lines = io.lines(path)
+function readFile(file)
+    local lines = io.lines(file)
     local result = {}
     
     for line in lines do 
         table.insert(result, line)
     end
     return result
+end
+
+function writeFile(file, text)
+    wrFile = io.open(file, "w")
+    io.output(wrFile)
+    io.write(text)
+    io.close(wrFile)
 end
