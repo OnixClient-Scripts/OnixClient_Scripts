58426ef29c1fc873bd9f2fe6bc23a2375511c7e0
HugoNotHere <80633305+HugoNotHere@users.noreply.github.com>
2021-09-12 11:33:40 +0100

    removed annoying module background
    to add to back change BackColor = { r=0, g=0, b=0, a=0 } to BackColor = { r=0, g=0, b=0, a=128 }

--- Modules/PingCounter.lua
+++ Modules/PingCounter.lua
@@ -2,7 +2,7 @@ name = "Ping Display"
 description = "tries its best to show your ping"
 
 TextColor = { r=255, g=255, b=255, a=255 }
-BackColor = { r=0, g=0, b=0, a=127 }
+BackColor = { r=0, g=0, b=0, a=0 }
 
 brackets = false
 
