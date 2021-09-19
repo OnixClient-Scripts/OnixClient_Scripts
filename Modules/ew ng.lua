78095b538332b2577933315bc108a1865db42620
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-09-19 22:59:10 +0900

    k fix

--- Modules/ew ng.lua
+++ Modules/ew ng.lua
@@ -8,13 +8,13 @@ description = "firetruck you ng"
     also made by rice
 ]]
 
-local ip = server.ip()
 
 function render(deltaTime)
-	if ip == ("play.nethergames.org") then
-            gfx.color (255,255,255,255)
-            gfx.text (0, 26, "because ng has stupid rules,\nyou can no longer use onix client on this server\n\nyou can press Ctrl + L to remove the client,\nor play a different server", 2)
-            gfx.color(0,0,0,255)
-            gfx.rect(0, 0, gui.width(), gui.height())
+    local ip = server.ip()
+    if ip == ("play.nethergames.org") then
+        gfx.color (255,255,255,255)
+        gfx.text (0, 26, "because ng has stupid rules,\nyou can no longer use onix client on this server\n\nyou can press Ctrl + L to remove the client,\nor play a different server", 2)
+        gfx.color(0,0,0,255)
+        gfx.rect(0, 0, gui.width(), gui.height())
     end
 end
