ad554c6d8672e4a75b8e9e6184cef976db257040
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-09-19 22:56:35 +0900

    Update ew ng.lua

--- Modules/ew ng.lua
+++ Modules/ew ng.lua
@@ -15,6 +15,6 @@ function render(deltaTime)
             gfx.color (255,255,255,255)
             gfx.text (0, 26, "because ng has stupid rules,\nyou can no longer use onix client on this server\n\nyou can press Ctrl + L to remove the client,\nor play a different server", 2)
             gfx.color(0,0,0,255)
-            gfx.rect(0, 0, gui.width(), gui,height)
+            gfx.rect(0, 0, gui.width(), gui.height())
     end
 end
