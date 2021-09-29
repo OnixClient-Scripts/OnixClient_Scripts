d65810d268ef01abf418384250e384899ff23c16
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-09-29 22:16:30 +0900

    fix for the worst update (1.17.30) and removed data
    fix for the worst update (1.17.30) and removed data file
    its useless

--- Modules/deathcoordinates.lua
+++ Modules/deathcoordinates.lua
@@ -26,11 +26,6 @@ waypoint_message = false
 
 check = false
 
-stylefile = io.open("deathcoordstyle.txt","w")
-io.output(stylefile)
-io.write(waypoint_style)
-io.close(stylefile)
-
 function update()
 
 end
@@ -52,29 +47,18 @@ function render()
         check = false
         if waypoint_style > 0 then
             if waypoint_style == 1 then
-                datafile = io.open("dc" .. worldName .. ".txt","w")
-                io.output(datafile)
-                io.write("Death\n" .. player_x .. "\n" .. player_y .. "\n" .. player_z .. "\n")
-                io.close(datafile)
                 client.execute("waypoint remove Death")
                 client.execute("waypoint add Death " .. player_x .. " " .. player_y .. " " .. player_z)
                 if waypoint_message == true then
                     print("§eWaypoint §aDeath §eadded")
                 end
             elseif waypoint_style == 2 then
-                datafile = io.open("dc" .. worldName .. ".txt","a")
-                io.output(datafile)
-                io.write("Death " .. current_time .. "\n" .. player_x .. "\n" .. player_y .. "\n" .. player_z .. "\n")
-                io.close(datafile)
                 client.execute("waypoint add \"Death " .. current_time .."\" " .. player_x .. " " .. player_y .. " " .. player_z)
                 if waypoint_message == true then
                     print("§eWaypoint §aDeath " .. current_time .. " §eadded")
                 end
             elseif waypoint_style == 3 then
                 datafile = io.open("dc" .. worldName .. ".txt","a")
-                io.output(datafile)
-                io.write(current_time .."\n" .. player_x .. "\n" .. player_y .. "\n" .. player_z .. "\n")
-                io.close(datafile)
                 client.execute("waypoint add \"" .. current_time .."\" " .. player_x .. " " .. player_y .. " " .. player_z)
                 if waypoint_message == true then
                     print("§eWaypoint §a" .. current_time .. " §eadded")
