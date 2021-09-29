14648563c760d3873a7a762df2020015c1470252
Quoty0 <48370588+Quoty0@users.noreply.github.com>
2021-09-29 22:00:47 +0900

    fix for the worst update (1.17.30)

--- Modules/deathcoordinates.lua
+++ Modules/deathcoordinates.lua
@@ -11,10 +11,10 @@ waypoint_message = false
 
         waypoint_style configs
         0: disable waypoint
-	1 (default): make a waypoint 'Death' (overwrites the old waypoint) 
-	2: make a waypoint 'Death [Current Time]
+        1 (default): make a waypoint 'Death' (overwrites the old waypoint) 
+        2: make a waypoint 'Death [Current Time]
         3: make a waypoint '[Current Time]'
-        
+
         example of [Current Time]: [08/21/21 12:30:45]
 
 
@@ -37,18 +37,18 @@ end
 
 function render()
     local player_x,player_y,player_z = player.position()
-	local attribs = player.attributes()
-	local health = attribs.id(7).value
-	local current_time = os.date("[%x %X]")
+    local attribs = player.attributes()
+    local health = attribs.name("minecraft:health").value
+    local current_time = os.date("[%x %X]")
     local worldName = server.worldName()
-	
-	if health > 0 then
-	    check = true
-	end
-	
-	if health == 0 and check == true then
-	    print("§cYou died! Your §4death coordinates §care:")
-	    print("§8(§7" .. player_x .. " " .. player_y .. " " .. player_z .. "§8)")
+
+    if health > 0 then
+        check = true
+    end
+
+    if health == 0 and check == true then
+        print("§cYou died! Your §4death coordinates §care:")
+        print("§8(§7" .. player_x .. " " .. player_y .. " " .. player_z .. "§8)")
         check = false
         if waypoint_style > 0 then
             if waypoint_style == 1 then
