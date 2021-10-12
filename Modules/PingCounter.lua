cff3b5ad19d382c2accf07cc1ef690785e6c0e2b
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2021-10-12 12:54:33 -0400

    update for new settings!

--- Modules/PingCounter.lua
+++ Modules/PingCounter.lua
@@ -1,16 +1,14 @@
 name = "Ping Display"
 description = "tries its best to show your ping"
 
-TextColor = { r=255, g=255, b=255, a=255 }
-BackColor = { r=0, g=0, b=0, a=0 }
-
-brackets = false
-
-
-positionX = 100
-positionY = 2
+positionX = 6
+positionY = 60
 sizeX = 35
 sizeY = 10
+scale = 1.0
+
+color = {255,255,255,255}
+background_color = {0,0,0,127}
 
 PingDelay = 250 --in ms, you will need to restart the LuaPingHelper.exe for it to apply
 
@@ -26,6 +24,9 @@ https://www.mediafire.com/file/37wm885qwyk3bjj/LuaPingHelper.exe/file
 
 
 -----script-code-----
+
+client.settings.addColor("Text Color", "color")
+client.settings.addColor("Background Color", "background_color")
 
 DelayFile = io.open("PingCounterDelay.txt", "w")
 io.output(DelayFile)
@@ -73,17 +74,13 @@ function update()
 end
 
 function render()
-    local text = ""
-	if brackets == true then
-		text = "[" .. CurrentPing .. "ms" .. "]"
-	else
-		text = CurrentPing .. "ms"
-	end
+    local text = CurrentPing .. "ms"
     local font = gui.font()
 
-    gfx.color(BackColor.r, BackColor.g, BackColor.b, BackColor.a)
+    gfx.color(background_color.r, background_color.g, background_color.b, background_color.a)
     gfx.rect(0,0,sizeX,sizeY)
 
-    gfx.color(TextColor.r, TextColor.g, TextColor.b, TextColor.a)
+    gfx.color(color.r, color.g, color.b, color.a)
     gfx.text((sizeX / 2) - (font.width(text) / 2), 5 - (font.height / 2), text, 1)
+
 end
