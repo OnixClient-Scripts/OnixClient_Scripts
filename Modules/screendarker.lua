7e8c2b75dea33fb656f880992e5ae7c6d2c275fc
NotShige <matthewechavez1@gmail.com>
2021-10-13 01:07:44 +0800

    Add Strength slider in screendarker module

--- Modules/screendarker.lua
+++ Modules/screendarker.lua
@@ -7,8 +7,13 @@ description = "Make screen darker (make a black dimm on your screen)"
     made by hugo
 ]]
 
+darkStrength = 40
+
+client.settings.addInt("Strength", "darkStrength", 1, 255)
+
+
 function render(deltaTime)
-    gfx.color(0,0,0,120)
+    gfx.color(0,0,0,darkStrength)
     gfx.rect(0, 0, 1000, 1000)
 
 end
