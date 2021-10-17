822ec0bd9d958c80821f81b6aceca8b7e5cab465
OnixClient_Scripts <48370588+OnixClient-Scripts@users.noreply.github.com>
2021-10-17 16:34:40 +0900

    Create guiscale.lua

---
+++ Modules/guiscale.lua
@@ -0,0 +1,27 @@
+try that
+name = "guiscale"
+description = "change guiscale"
+
+
+--[[
+    Guiscale Module Script
+    
+    made by MCBE Craft
+]]
+
+client.execute("guiscale 1")
+scale = 1.0
+currentscale = 1.0
+
+client.settings.addFloat("Gui scale", "scale", 0.250, 20.0)
+
+function update()
+    if scale ~= currentscale then
+        client.execute("guiscale " .. scale)
+        currentscale = scale
+    end
+end
+
+function render()
+    
+end
