18cdf448ea0d2d5bf5518dc8136b5ab7cd26b693
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2021-11-03 09:17:46 -0400

    i feelt bad that this guiscale module was so bad.
    so i made a new one
    it will save guiscale and apply it when u resize your window
    it also has things like rounding the value or an apply button
    since scripts dont get updates while in client ui

--- Modules/guiscale.lua
+++ Modules/guiscale.lua
@@ -1,26 +1,27 @@
 name = "guiscale"
-description = "change guiscale"
+description = "changes and save your custom guiscale"
 
 
---[[
-    Guiscale Module Script
-    
-    made by MCBE Craft
-]]
 
-client.execute("guiscale 1")
-scale = 1.0
-currentscale = 1.0
 
-client.settings.addFloat("Gui scale", "scale", 0.250, 20.0)
+targetGuiscale = gui.scale()
+client.settings.addFloat("Guiscale", "targetGuiscale", 0.25, 15)
 
-function update()
-    if scale ~= currentscale then
-        client.execute("guiscale " .. scale)
-        currentscale = scale
-    end
+function apply()
+    client.execute("guiscale " .. targetGuiscale)
 end
 
-function render()
-    
+function roundd()
+    targetGuiscale = math.floor(targetGuiscale+0.5)
+    client.execute("guiscale " .. targetGuiscale)
+    client.settings.update()
+end
+
+client.settings.addFunction("You need to apply as your in the gui.", "apply", "Apply")
+client.settings.addFunction("Round the current guiscale", "roundd", "Round")
+
+function update(dt)
+    if (gui.scale() ~= targetGuiscale) then
+        client.execute("guiscale " .. targetGuiscale)
+    end
 end
