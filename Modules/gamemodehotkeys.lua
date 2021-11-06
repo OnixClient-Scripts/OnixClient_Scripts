3e09b415bd14a65dcf10a0f5524a7b4be8f8c3c7
Quoty0 <62657139+Quoty0@users.noreply.github.com>
2021-11-06 14:20:34 +0900

    Update and rename GamemodeSwitcherHotkey.lua to gamemodehotkeys.lua

---
+++ Modules/gamemodehotkeys.lua
@@ -0,0 +1,28 @@
+name = "Gamemode Hotkey"
+description = "Press hotkey to switch between Gamemode 0 and 1"
+
+--[[
+    Gamemode Hotkey Module Script
+    made by Hitu1197
+]]
+
+
+keybind = 0x47
+client.settings.addKeybind("Key", "keybind")
+
+function keyboard(key, isDown)
+    local isMouseGrabbed = gui.mouseGrabbed()
+	local gamemode = player.gamemode()
+
+    if (gamemode == 5) then
+        gamemode = 1
+    end
+ 
+    if isMouseGrabbed == false and key == keybind and isDown == true then
+        if gamemode == 1 then
+            client.execute("execute /gamemode s") 
+        elseif gamemode == 0 then
+            client.execute("execute /gamemode c")
+        end
+    end
+end
