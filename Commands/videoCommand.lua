0e2fa7b90a79bddee5b191404eb07f6addb58a62
MCBE Craft <hug.dp0@gmail.com>
2021-12-30 22:27:36 +0100

    :)

---
+++ Commands/videoCommand.lua
@@ -0,0 +1,18 @@
+command = "video"
+help_message = "plays videos if sound pack is on and frames in data folder"
+
+ImportedLib = importLib("readfile.lua")
+
+function execute(arguments)
+    if arguments == "reset" then
+        writeFile("video.txt", "")
+        print("Stopped playing")
+    elseif arguments == "stop" then
+        writeFile("video.txt", "")
+        print("Stopped playing")
+    else
+        writeFile("video.txt", arguments)
+        gui.sound("record." .. arguments)
+        print("Now playing: " .. arguments)
+    end
+end
