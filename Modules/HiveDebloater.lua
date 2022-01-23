62a01de07d5c33bc16534a26a6d85986b64a57d8
jams <65556454+jamsuwu@users.noreply.github.com>
2022-01-23 14:06:16 +0000

    v2.0 of hive debloater
    added aesthetic changes to the menu + a few more things to remove

--- Modules/HiveDebloater.lua
+++ Modules/HiveDebloater.lua
@@ -1,5 +1,5 @@
 name = "Hive Chat Debloater"
-description = "Adds small aesthetic tweaks to §eThe Hive§r's chat (debloats).\n§o§l§0Release v1.2\n§rMade by Jams\n\nExtra: .block command. (Requires Blocker.lua & Blockernt.lua)"
+description = "Adds small aesthetic tweaks to §eThe Hive§r's chat (debloats).                                      §o§l§8Release §0v2.0\n§rExtra: .block command. (Requires §7Blocker.lua§r &§7 Blockernt.lua)                                    §r§7Made by §eJams\nHeavy big thank to Mr. Onix!"
 
 --[[
     Hive Chat Debloater Script
@@ -17,6 +17,7 @@ RemoveTeamers = false
 RemoveVoting = false
 RemoveEliminated = false
 RemoveLoginStreak = false
+RemovePlayerJoined = false
 
 function Debloat()
     RemoveUnlocks = true
@@ -27,6 +28,7 @@ function Debloat()
     RemoveVoting = true
     RemoveEliminated = true
     RemoveLoginStreak = true
+    RemovePlayerJoined = true
     client.settings.send()
 end
 function Bloat()
@@ -38,49 +40,85 @@ function Bloat()
     RemoveVoting = false
     RemoveEliminated = false
     RemoveLoginStreak = false
+    RemovePlayerJoined = false
     client.settings.send()
 end
 
+function update(dt)
+    local GuiWindowWidth = ((gui.width() - (gui.width() * 0.05)) - ((gui.width() / 2) + 5)) - 16
+    SplitterText = "━"
+    local CurrentClientFont = gui.font()
+    while CurrentClientFont.width(SplitterText, 0.8) < GuiWindowWidth do
+        SplitterText = SplitterText .. "━"
+    end
+end
+SplitterText = "━"
+update()
 
-client.settings.addAir(10)
+client.settings.addInfo("SplitterText")
+
+client.settings.addAir(0)
 client.settings.addFunction("Enable All", "Debloat", "Debloat")
 client.settings.addFunction("Disable All", "Bloat", "Bloat")
-client.settings.addAir(10)
+client.settings.addAir(0)
+
+client.settings.addInfo("SplitterText")
 
 client.settings.addBool("Remove login streak message.", "RemoveLoginStreak")
 noLoginStreak = "Removes:\n§8§l[§a§lLR§8§l] §rYou're now on a (day) day login streak. XP boost: +(XP boost)%"
 client.settings.addInfo("noLoginStreak")
-client.settings.addAir(10)
+client.settings.addAir(0)
+
+client.settings.addInfo("SplitterText")
 
 client.settings.addBool("Remove \"You have unused unlocks\" message.", "RemoveUnlocks")
 noUnlocks = "Removes:\n§a§l» §rYou have unused unlocks in your Locker!"
 client.settings.addInfo("noUnlocks")
-client.settings.addAir(10)
+client.settings.addAir(0)
+
+client.settings.addInfo("SplitterText")
 
 client.settings.addBool("Remove \"You have been hidden\" message.", "RemoveHiddenMessage")
 noHiddenMessage = "Removes:\n§b§l» §r§7You have been hidden. §8[Hidden Zone]\n§b§l» §r§7You are now visible."
 client.settings.addInfo("noHiddenMessage")
-client.settings.addAir(10)
+client.settings.addAir(0)
+
+client.settings.addInfo("SplitterText")
 
 client.settings.addBool("Remove \"No teaming!\" message.", "RemoveTeamers")
 noTeaming = "Removes:\n§c§l» §r§c§lNo teaming! §r§6Teamers will be banned."
 client.settings.addInfo("noTeaming")
-client.settings.addAir(10)
+client.settings.addAir(0)
+
+client.settings.addInfo("SplitterText")
 
 client.settings.addBool("Remove \"Voting has ended\" message.", "RemoveVoting")
 noVoting = "Removes:\n§b§l» §r§a§lVoting has ended!"
 client.settings.addInfo("noVoting")
-client.settings.addAir(10)
+client.settings.addAir(0)
+
+client.settings.addInfo("SplitterText")
 
 client.settings.addBool("Remove \"Gameover\" message.", "RemoveGameOver")
 noGameover = "Removes:\n§c§l» §r§c§lGame OVER!"
 client.settings.addInfo("noGameover")
-client.settings.addAir(10)
+client.settings.addAir(0)
+
+client.settings.addInfo("SplitterText")
 
 client.settings.addBool("Remove Eliminated text", "RemoveEliminated")
-wasEliminated = "Removes:\n§7(Colour)§r was ELIMINATED!"
+wasEliminated = "Removes:\n§7§e(Colour)§r has been §cELIMINATED!"
 client.settings.addInfo("wasEliminated")
-client.settings.addAir(10)
+client.settings.addAir(0)
+
+client.settings.addInfo("SplitterText")
+
+client.settings.addBool("Remove Player joined message", "RemovePlayerJoined")
+playerJoined = "Removes:\n§a§l» §r§7§e(Player) §7joined. §8[§e(Amount of players)§8]"
+client.settings.addInfo("playerJoined")
+client.settings.addAir(0)
+
+client.settings.addInfo("SplitterText")
 
 client.settings.addBool("Remove Tips", "RemoveTips")
 serverMessages = "Removes the following server messages:\n\n§6[§e!§6] §r§7Didn't get your store purchase? §3Try §a/fixpurchase§r,\n§6[§e!§6] §r§aJoin our forums : §3Go to §bforum.playhive.com§r,\n§6[§e!§6] §r§9Need help? §3Go to §asupport.playhive.com§r,\n§6[§e!§6] §r§9Join our Discord : run §d/discord§r,\n§6[§e!§6] §r§aFollow our Twitter : §b@theHiveMC§r."
@@ -135,11 +173,15 @@ function onChat(message, username, type)
     return true
     end
 
-    if RemoveEliminated == true and string.find(message, "was ELIMINATED!") ~= nil then
+    if RemoveEliminated == true and string.find(message, "has been §cELIMINATED§7!") ~= nil then
     return true
     end
 
     if RemoveLoginStreak == true and string.find(message, "day loginstreak. XP boost: +") ~= nil then
+    return true
+    end
+
+    if RemoveLoginStreak == true and string.find(message, "§a§l»".."joined.") ~= nil then
     return true
     end
 
@@ -149,7 +191,6 @@ function onChat(message, username, type)
             return true
         end
     end
-    
     local file = io.open("Chatlog.txt", 'a')
     file:write(message .. "\n")
     io.close(file)
