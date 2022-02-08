cd8f2260152c87202420af86d1b58b4d028af845
MCBE Craft <mcbe.craft0@gmail.com>
2022-02-08 09:00:36 +0100

    fixed and improved scope module

--- Modules/scope.lua
+++ Modules/scope.lua
@@ -5,17 +5,52 @@ description = "adds a scope do bows and crossbows"
     Scope Module Script
 	
 	made by MCBE Craft
+
+    !!! NEEDS A "scope.png" FILE IN THE DATA FOLDER !!!
 ]]
 
---!!! NEEDS A "scope.png" FILE IN THE DATA FOLDER !!!
-bowId = "bow"
-crossbowId = "crossbow"
-arrowId = 301
+
+bowId = 300
+crossbowId = 575
+
+positionX = 0
+positionY = 0
+sizeImage = 50
+sizeX = 0
+sizeY = 0
+
+
+sizeHeight = false
+client.settings.addAir(5)
+client.settings.addBool("Make the scope render across the width", "sizeHeight")
+
+
+crossbows = true
+client.settings.addAir(5)
+client.settings.addBool("Adds the scope to crossbows too", "crossbows")
+
+
+
+
+function update(deltaTime)
+    local width = gui.width()
+    local height = gui.height()
+    if sizeHeight then
+        sizeImage = width
+        positionX = 0
+        positionY = (height / 2) - (width / 2)
+    else
+        sizeImage = height
+        positionX = (width / 2) - (height / 2)
+        positionY = 0
+    end
+    
+end
 
 function render(deltaTime)
     local inventory = player.inventory()
     local selected = inventory.at(inventory.selected)
-    if ((selected ~= nil and (selected.name == bowId or selected.name == crossbowId))) then
-        gfx.image(0, 0, gui.width(), gui.height(), "scope.png")
+    if ((selected ~= nil and (selected.id == bowId or crossbows and selected.id == crossbowId))) then
+        gfx.image(0, 0, sizeImage, sizeImage, "scope.png")
     end
 end
