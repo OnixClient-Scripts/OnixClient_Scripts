0e9dd3224bc6128ae992d548f8f76f43adca1516
EpiclyRaspberry <84133368+EpiclyRaspberry@users.noreply.github.com>
2022-03-10 01:10:33 +0800

    idk i added something so its gonna be clear to use
    (idea clearly not taken from onix's autocomplete)

--- Libs/renderthreeD.lua
+++ Libs/renderthreeD.lua
@@ -1,10 +1,11 @@
+---Use to render a cube in a world at predermined position and size
+---@param x number x position of the cube
+---@param y number y position of the cube
+---@param z number z position of the cube
+---@param s number size of the cube
+---@return nil
 function cube(x,y,z,s)
-    --[[
 
-     function cube(x,y,z,s)
-    use to render a cube in a world at predermined position and size
-
-    ]]--
     gfx.quad(x,y,z,x+s,y,z,x+s,y+s,z,x,y+s,z,true)
     gfx.quad(x,y,z+s,x+s,y,z+s,x+s,y+s,z+s,x,y+s,z+s,true)
     gfx.quad(x,y,z,x,y,z+s,x,y+s,z+s,x,y+s,z,true)
@@ -12,13 +13,16 @@ function cube(x,y,z,s)
     gfx.quad(x,y,z,x+s,y,z,x+s,y,z+s,x,y,z+s,true)
     gfx.quad(x,y+s,z,x+s,y+s,z,x+s,y+s,z+s,x,y+s,z+s,true)
 end
+---use to render a cube in a world at predermined position and size
+---@param x number x position of the cube
+---@param y number y position of the cube
+---@param z number z position of the cube
+---@param sx number x size of the cube
+---@param sy number y size of the cube
+---@param sz number z size of the cube
+---@return nil
 function cubexyz(x,y,z,sx,sy,sz)
-    --[[
 
-     function cubexyz(x,y,z,sx,sy,sz)
-    use to render a cube in a world at predermined position and size
-
-    ]]--
     gfx.quad(x,y,z,x+sx,y,z,x+sx,y+sy,z,x,y+sy,z,true)
     gfx.quad(x,y,z+sz,x+sx,y,z+sz,x+sx,y+sy,z+sz,x,y+sy,z+sz,true)
     gfx.quad(x,y,z,x,y,z+sz,x,y+sy,z+sz,x,y+sy,z,true)
@@ -26,13 +30,15 @@ function cubexyz(x,y,z,sx,sy,sz)
     gfx.quad(x,y,z,x+sx,y,z,x+sx,y,z+sz,x,y,z+sz,true)
     gfx.quad(x,y+sy,z,x+sx,y+sy,z,x+sx,y+sy,z+sz,x,y+sy,z+sz,true)
 end
-
+---use to render a cube's frame in a world at predermined position and size
+---@param x number x position of the cube
+---@param y number y position of the cube
+---@param z number z position of the cube
+---@param s number size of the cube
+---@param t number thickness of the frame
+---@return nil
 function cubeframe(x,y,z,s,t)
-    --[[
     
-         function cubeframe(x,y,z,s,t)
-    use to render a cube's frame in a world at predermined position and size
-    ]]--
     cubexyz(x,y,z,s/16/(t/16/16)*(t/5.30),s/16/(t/16),s/16/(t/16)) --base x
     cubexyz(x,y,z,s/16/(t/16),s/16/(t/16),s/16/(t/16/16)*(t/5.30)) --base z
     cubexyz(x,y,z,s/16/(t/16),s/16/(t/16/16)*(t/5.30),s/16/(t/16)) --base y
