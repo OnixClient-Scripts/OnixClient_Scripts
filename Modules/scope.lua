a17fef370fff0bb92fcb612e6b8732d13f52881c
MCBE Craft <mcbe.craft0@gmail.com>
2022-03-09 15:06:49 +0100

    Update scope.lua

--- Modules/scope.lua
+++ Modules/scope.lua
@@ -13,11 +13,9 @@ description = "adds a scope do bows and crossbows"
 bowId = 300
 crossbowId = 575
 
-positionX = 0
-positionY = 0
+posX = 0
+posY = 0
 sizeImage = 50
-sizeX = 0
-sizeY = 0
 
 
 sizeHeight = false
@@ -37,12 +35,12 @@ function update(deltaTime)
     local height = gui.height()
     if sizeHeight then
         sizeImage = width
-        positionX = 0
-        positionY = (height / 2) - (width / 2)
+        posX = 0
+        posY = (height / 2) - (width / 2)
     else
         sizeImage = height
-        positionX = (width / 2) - (height / 2)
-        positionY = 0
+        posX = (width / 2) - (height / 2)
+        posY = 0
     end
     
 end
@@ -51,6 +49,6 @@ function render(deltaTime)
     local inventory = player.inventory()
     local selected = inventory.at(inventory.selected)
     if ((selected ~= nil and (selected.id == bowId or crossbows and selected.id == crossbowId))) then
-        gfx.image(0, 0, sizeImage, sizeImage, "scope.png")
+        gfx.image(posX, posY, sizeImage, sizeImage, "scope.png")
     end
 end
