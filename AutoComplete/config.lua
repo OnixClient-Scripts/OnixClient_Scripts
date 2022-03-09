44069e98eccee51e0e3764e4e59e98f5fe11ae84
MCBE Craft <mcbe.craft0@gmail.com>
2022-03-09 17:54:40 +0100

    onix forgor

--- AutoComplete/config.lua
+++ AutoComplete/config.lua
@@ -1,5 +1,5 @@
 -- if not set, the folder name will be used
 name    = 'Onix'
 -- list of matched words
-words   = {"name=", "description=", "help_message=", "command=", "name = ", "description = ", "help_message = ,command = "}
+words   = {"name=", "description=", "help_message=", "command=", "name = ", "description = ", "help_message = ", "command = "}
 -- list or matched file names. `.lua`, `.dll` and `.so` only
