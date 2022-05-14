874b7668aaf408046f6ccde96af860972ffed46e
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2022-05-14 19:48:04 -0400

    fix lua error/warning

--- AutoComplete/library/gui.lua
+++ AutoComplete/library/gui.lua
@@ -1,106 +1,225 @@
 ---@meta
 
----@class gui
-gui = {}
+---@class player
+player = {}
+
+---The current gamemode of the player
+---@return integer gamemode
+---```
+---0 = Survival
+---1 = Creative
+---2 = Adventure
+---3 = SurvivalViewer
+---4 = CreativeViewer
+---5 = Default
+---```
+function player.gamemode() end
+
+---The position of the player
+---@return integer x
+---@return integer y
+---@return integer z
+function player.position() end
+
+---The coordinates of the block that has the outline for the player
+---You can check if there is one in the first place with player.facingBlock()
+---@return integer x
+---@return integer y
+---@return integer z
+function player.selectedPos() end
+
+---Where the raytrace hit, exemple: you look at a block far away, where on that block are you looking at
+---@return number x
+---@return number y
+---@return number z
+function player.lookingPos() end
+
+---Where the player looks at
+---@return number yaw
+---@return number pitch
+function player.rotation() end
+
+---If the player were to left click would it attack an entity
+---@return boolean
+function player.facingEntity() end
+
+---If the player were to left click would it attack a block
+---@return boolean
+function player.facingBlock() end
+
+---The xbox pro gamering tag
+---@return string xboxName
+function player.name() end
+
+---empty in vanilla, servers can set a custom nametag there
+---@return string nametag
+function player.nametag() end
 
 
----The width of the screen
----@return number
-function gui.width() end
----The height of the screen
----@return number
-function gui.height() end
----The minecraft guiscale
----@return number
-function gui.scale() end
----If the user move the mouse it will affect the game or the ui (true = ui)
----@return boolean mouseGrabbed
-function gui.mouseGrabbed() end
-
----Set the mouseGrabbed state
----@param grabbed boolean true will not allow the player to interact with the world
----@return nil
-function gui.setGrab() end
-
----The position of the mouse on the X axis (left to right)
----@return number mouseX
-function gui.mousex() end
----The position of the mouse on the Y axis (left to right)
----@return number mouseY
-function gui.mousey() end
-
----Opens a client screen, exemple "HudEditor"
----@param name string | "\"HudEditor\"" | "\"ModuleEditor\"" | "\"CrosshairPainter\"" | "\"ThemeEditor\""
----@return boolean screenShowed
-function gui.showScreen(name) end
-
----Plays the click sound (the one in minecraft when u clicc button)
----@return nil
-function gui.clickSound() end
-
----Plays a sound on the ui (will not go away if you move)
----@param name string [any minecraft sounds](https://www.digminecraft.com/lists/sound_list_pe.php)
----@return nil
-function gui.sound(name) end
-
----Stops all sound that are playing
----@return nil
-function gui.stopallsound() end
-
----Stops all sound that are playing
----@return nil
-function gui.stopallsound() end
-
----Stops all sound that are playing
----@return Theme
-function gui.theme() end
-
----Stops all sound that are playing
----@return Font
-function gui.font() end
-
-
----@class iColor
----@field r integer
----@field g integer
----@field b integer
----@field a integer
-local iColor = {}
-
----@class Theme
----@field back iColor the background color, whats below everything
----@field moduleOutline iColor the outline of the visual modules in the hud editor
----@field moduleOverlay iColor the overlay on top of the visual modules in the hud editor
----@field darkbutton iColor most buttons use "enabled" but some use darkbutton's color instead
----@field text iColor color of the text on the ui
----@field largeArea iColor color of the body of a window/pannel
----@field titlebar iColor color of the titlebar
----@field disabled iColor color of disabled stuff
----@field enabled iColor color of enabled stuff, can be used as accent color
----@field blocked iColor blocked content in that color
-local _acp_theme = {}
-
-
-
----@class Font
----@field isMinecrafttia boolean Is the minecraft blocky font in use
----@field height number the height of one char
----@field wrap number the height of one line
-local _acp_font = {}
-
----Returns the size of the text
----@param text string The text to get the size of
----@return number widthOfText
-function _acp_font.width(text) end
-
----Returns the size of the text
----@param text string The text to get the size of
----@param scale number The scale
----@return number widthOfText
-function _acp_font.width(text, scale)
+---Returns if  true if the flag is true
+---@param flag integer The flag to check
+---@return boolean flag
+---```
+---0: --on fire
+---1: --is sneaking
+---2: --riding an entity
+---3: --sprinting
+---4: --using an item
+---5: --invisible (example: potion)
+---14: --can show nametag
+---15: --always show nametag
+---16: --immobile (when player is dead or unable to move)
+---19: --can climb (if the player can use ladders)
+---32: --gliding with elytra
+---38: --if player is moving
+---39: --if player is breathing
+---47: --has collision with other entities
+---48: --has gravity
+---49: --immune to fire/lava damage
+---52: --returning loyalty trident
+---55: --doing spin attack with riptide trident
+---56: --swimming
+---68: --inside a scaffolding block
+---69: --on top of a scaffolding block
+---70: --falling through a scaffolding block
+---71: --blocking (using shield or riding a horse? confused about how this one gets triggered)
+---72: --transition blocking (same idea as 71)
+---73: --blocked from entity using a shield
+---74: --blocked from entity using a damaged shield (why does this exist?)
+---75: --sleeping
+---88: --if the player should render when they have the invisibility status effect
+---
+---
+---Do note that status flags are sent by the SERVER. Thus, many custom servers may only send essential flags such as on fire or sneaking. However, more niche flags are expected to be sent by the vanilla bedrock server. The other status flags do not apply to players and are thus omitted from the documentation (for now)
+---```
+function player.getFlag(flag) end
 
 
 
+---@class Effect
+---@field duration number How long the effect will last in seconds
+---@field level integer How strong the effect will be: ex Strenght II
+---@field id integer The numerical identifier of the effect
+---@field effectTimeText string The formated text of the time remaining (ex: 6:45)
+---@field name string The name of the effect ex: night_vision
+---@field vname string The visual name ex: Night Vision II
+
+
+---@return Effect[] effects
+function player.effects() end
 
 
 
+---@class AttributeListHolder
+---@field size integer
+local _acp_AttributeListHolder = {}
+
+---@class Attribute
+---@field name string The name of the attribute
+---@field id integer The numerical identifier of the attribute
+---@field value number The value of the attribute
+---@field default number The default value of the attribute
+---@field min number The minimum value of the attribute
+---@field max number The maximum value of the attribute
+
+---Gets the attribute with this name or nil
+---@param attribute_name string | "\"minecraft:player.hunger\"" | "\"minecraft:player.saturation\"" | "\"minecraft:player.exhaustion\"" | "\"minecraft:player.level\"" | "\"minecraft:player.experience\"" | "\"minecraft:health\"" | "\"minecraft:follow_range\"" | "\"minecraft:knockback_resistance\"" | "\"minecraft:movement\"" | "\"minecraft:underwater_movement\"" | "\"minecraft:lava_movement\"" | "\"minecraft:attack_damage\"" | "\"minecraft:absorption\"" | "\"minecraft:luck\""
+---@return Attribute attrib
+function _acp_AttributeListHolder.name(attribute_name) end
+
+
+---Gets the attribute with this id or nil
+---@param attribute_id integer the attribute id
+---```
+---2: --Hunger
+---3: --Saturation
+---4: --Exhaustion
+---5: --Level
+---6: --Experience
+---7: --Health
+---9: --Knockback Resistance
+---10: --Movement Speed
+---11: --Underwater Speed
+---12: --Lava Speed
+---13: --Attack Damage
+---14: --Absorption
+---15: --Luck
+---```
+function _acp_AttributeListHolder.id(attribute_id) end
+
+---Gets the attribute at this position in the list or nil
+---@param position integer 
+---```
+--- --you can iterate trough all of them by doing this:
+---for i,attributes.size do
+---	print(attributes.at(i).id .. ": " .. attributes.at(i).name)
+---end
+---```
+function _acp_AttributeListHolder.at(position) end
+
+
+---Gets the attributes
+---@return AttributeListHolder attribs
+function player.attributes() end
+
+
+
+
+
+
+---@class Enchants
+---@field id integer The numerical identifier ex: 9
+---@field level integer the level, ex: 1
+---@field name string The name (like in /enchant) ex: sharpness
+local _acp_Enchants = {}
+
+
+---@class Item
+---@field count integer The amount of items in this stack
+---@field location integer dont touch this dont guess it use it when referencing an item to client functions
+---@field id integer The runtime numerical identifier of the item, will change often use for runtime only
+---@field name string The name of the item  ex: diamond_sword
+---@field maxStackCount integer The maximum amount of this item that can be stacked
+---@field maxDamage integer The maximum durability of an item
+---@field durability integer The amount of damage applied to that item
+---@field data integer The data of the item, like in /give with dye and things like that
+---@field customName string The name (ex: from anvils)
+---@field enchant Enchants[]
+
+
+---@class InventoryArmor 
+---@field helmet Item|nil the item on the head
+---@field chestplace Item|nil the item on the torso
+---@field leggings Item|nil the item on the legs
+---@field boots Item|nil the item on the feets
+
+---@class Inventory
+---@field size integer The size of the inventory
+---@field selected integer The selected slot in the inventory (hotbar)
+local _acp_Inventory = {}
+
+---The armor inventory
+---@return InventoryArmor
+function _acp_Inventory.armor() end
+
+---The item in the second hand
+---@return Item|nil
+function _acp_Inventory.offhand() end
+
+---The item in slot or nil
+---@param slot integer The inventory slot
+---@return Item|nil
+---```
+--- --proper iteration (checking for nil for every item you get)
+---for i=1,inventory.size do
+---	local item = inventory.at(i)
+---	if item ~= nil then
+---		print(item.name)
+---	end
+---end
+---```
+function _acp_Inventory.at(slot) end
+
+---Gets the inventory of the player
+---@return Inventory
+function player.inventory() end
