671592a5f277dc5afb7c11f857800094855028b5
rosie <65556454+jqms@users.noreply.github.com>
2022-06-28 23:03:33 +0100

    added gamemode lobbies

--- Modules/HiveRPC.lua
+++ Modules/HiveRPC.lua
@@ -16,7 +16,9 @@ importLib("DependentBoolean")
 
 lastGamemode = ""
 prefix = ""
+suffix = "lobby."
 
+lobby = false
 displaySettings = false
 displayGamemode = false
 displayUsername = false
@@ -28,8 +30,8 @@ client.settings.addDependentBool("Display Username?","displayUsername","displayS
 local formattedGamemodes = {
     DROP="Block Drop",
     CTF="Capture The Flag",
-    BRIDGE="The §5Bridge",
-    GROUND="Ground §2Wars",
+    BRIDGE="The Bridge",
+    GROUND="Ground Wars",
     SG="Survival Games",
     MURDER="Murder Mystery",
     WARS="Treasure Wars",
@@ -38,7 +40,8 @@ local formattedGamemodes = {
     HIDE="Hide And Seek",
     DR="DR",
     ARCADE="Arcade Hub",
-    HUB="Hub"
+    HUB="Hub",
+    REPLAY="Replay"
 }
 formattedGamemodes["BRIDGE-DUOS"]="The Bridge: Duos" 
 formattedGamemodes["SG-DUOS"]="Survival Games: Duos"
@@ -57,44 +60,67 @@ formattedGamemodes["BUILDX-DUOS"]="Just Build: Extended, Duos"
 
 function update(dt)
 	client.settings.updateDependencies()
-    
-    if formattedGamemode ~= "Hub" then
+
+    if  lobby == false and formattedGamemode ~= "Hub" and lobby == false then
         prefix = "Playing "
     else
-        if formattedGamemode == "Hub" then
-            prefix = "In the "
+        prefix = "In the "
+    end
+    if  lobby == false and formattedGamemode ~= "Replay" and formattedGamemode ~= "Hub" then
+        prefix = "Playing "
+    else
+        if  lobby == false and formattedGamemode == "Replay" then
+            prefix = "In a "
         end
     end
 
     if formattedGamemode == nil then
         return
-    else
-        if displayGamemode == true and displaySettings == true then
+    elseif lobby == false and displayGamemode == true and displaySettings == true then
             file = io.open("HiveRPCHelperGamemode.txt", "w")
             io.output(file)
             io.write("" ..  prefix .. formattedGamemode)
             io.close(file)
-        else
+        elseif displayGamemode == false then
             file = io.open("HiveRPCHelperGamemode.txt", "w")
             io.output(file)
             io.write("")
             io.close(file)
-        end
     end
+    if formattedGamemode == nil then
+        return
+    elseif lobby == true then
+        file = io.open("HiveRPCHelperGamemode.txt", "w")
+        io.output(file)
+        io.write("In a " .. formattedGamemode .. " lobby")
+        io.close(file)
+    end
+
     local username = player.name()
     if displayUsername == true and displaySettings == true then
         file1 = io.open("HiveRPCHelperUsername.txt", "w")
         io.output(file1)
         io.write("As " ..  username)
         io.close(file1)
-    else
+    elseif displayUsername == false then
         file1 = io.open("HiveRPCHelperUsername.txt", "w")
         io.output(file1)
         io.write("")
         io.close(file1)
     end
+
 end
+
 function onChat(message, username, type)
+
+    if string.find(message,"joined. §8") then
+        lobby = true
+        return
+    elseif string.find(message, "§b§l» §r§a§lVoting has ended!") then
+        lobby = false
+        return
+    end
+
     if string.find(message, "§b§l» §r§a§lVoting has ended!") or string.find(message," joined. §8") or string.find(message, "§a") then
         client.execute("execute /connection")
     end
