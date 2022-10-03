c00582b774dc87ad1cd4a6335bce8221ae3f40ef
OnixClient_Scripts <48370588+OnixClient-Scripts@users.noreply.github.com>
2022-10-04 04:55:31 +0900

    Delete AutoComplete directory

--- AutoComplete/config.lua
+++
@@ -1,5 +0,0 @@
--- if not set, the folder name will be used
-name    = 'Onix'
--- list of matched words
-words   = {"name=", "description=", "help_message=", "command=", "name = ", "description = ", "help_message = ", "command = "}
--- list or matched file names. `.lua`, `.dll` and `.so` only
