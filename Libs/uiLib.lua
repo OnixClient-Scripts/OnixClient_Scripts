ce8878690b62c2640d43fd1b47f9abf525234a84
MCBE Craft <mcbe.craft0@gmail.com>
2022-10-07 16:27:31 +0200

    fix

--- Libs/uiLib.lua
+++ Libs/uiLib.lua
@@ -6,7 +6,7 @@ importLib("keyconverter.lua")
 
 local keyToAction = {}
 
-for k, v in pairs(string.split("65=A,32= ,66=B,0x43=C,67=D,69=E,70=F,71=G,72=H,73=I,74=J,75=K,76=L,77=M,78=N,79=O,80=P,81=Q,82=R,83=S,84=T,85=U,86=V,87=W,88=X,89=Y,90=Z,48=0,49=1,50=2,51=3,52=4,53=5,54=6,55=7,56=8,57=9", ",")) do
+for k, v in pairs(string.split("65=A,32= ,66=B,67=C,68=D,69=E,70=F,71=G,72=H,73=I,74=J,75=K,76=L,77=M,78=N,79=O,80=P,81=Q,82=R,83=S,84=T,85=U,86=V,87=W,88=X,89=Y,90=Z,48=0,49=1,50=2,51=3,52=4,53=5,54=6,55=7,56=8,57=9", ",")) do
     local splitted = string.split(v, "=")
     keyToAction[splitted[1]] = splitted[2]
 end
