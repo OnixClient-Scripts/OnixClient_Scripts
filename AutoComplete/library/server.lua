41ffd8e0d6069a345f193182b9b6e6f100159bf5
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2022-11-20 18:33:37 -0500

    Update server.lua

--- AutoComplete/library/server.lua
+++ AutoComplete/library/server.lua
@@ -4,30 +4,27 @@
 server = {}
 
 
----The ip that you are currently exchanging packets with
----@return string ip
+---The current ip the user is connected to
+---@return string ip The current ip the user is connected to
 function server.ipConnected() end
 
 ---The ip that you used to join the server
----@return string ip
+---@return string ip The ip used to join the server
 function server.ip() end
 
 ---The port
----@return integer port
+---@return integer port The current server port
 function server.port() end
 
 ---The name of the world (whats in top right in pause screen)
----@return string name
+---@return string name The world name
 function server.worldName() end
 
 
 ---The name of everyone in the pause menu
----@return string[] playernames
+---example of usage:
+---for _, playerName in pairs(server.players()) do
+---
+---end
+---@return string[] playernames The name of everybody in the playerlist (same as pause menu)
 function server.players() end
-
-
-
-
-
-
-
