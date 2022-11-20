1b18eaebac7223fe0f5a1616c8968af095bc6021
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2022-11-20 18:32:50 -0500

    Update gui.lua

--- AutoComplete/library/gui.lua
+++ AutoComplete/library/gui.lua
@@ -6,62 +6,62 @@ gui = {}
 
 
 ---The width of the screen
----@return number
+---@return number width The width of the screen
 function gui.width() end
 ---The height of the screen
----@return number
+---@return number height The height of the screen
 function gui.height() end
 ---The minecraft guiscale
----@return number
+---@return number scale The minecraft guiscale
 function gui.scale() end
 ---If the user move the mouse it will affect the game or the ui (true = ui)
----@return boolean mouseGrabbed
+---@return boolean mouseGrabbed Will be false if in the world
 function gui.mouseGrabbed() end
 
 ---Set the mouseGrabbed state
 ---@param grabbed boolean true will not allow the player to interact with the world
----@return nil
 function gui.setGrab(grabbed) end
 
 ---The position of the mouse on the X axis (left to right)
----@return number mouseX
+---@return number mouseX The position of the mouse on the X axis
 function gui.mousex() end
 ---The position of the mouse on the Y axis (left to right)
----@return number mouseY
+---@return number mouseY The position of the mouse on the Y axis
 function gui.mousey() end
 
----Opens a client screen, exemple "HudEditor"
+---Opens a client screen, examples:
+---"HudEditor"
+---"ModuleEditor"
+---"CrosshairPainter"
+---"ThemeEditor"
 ---@param name string | "\"HudEditor\"" | "\"ModuleEditor\"" | "\"CrosshairPainter\"" | "\"ThemeEditor\""
----@return boolean screenShowed
+---@return boolean screenShowed If the screen was showed
 function gui.showScreen(name) end
 
 ---Gives you the name of the current minecraft screen
----@return string screenName
+---@return string screenName The name of the current screen
 function gui.screen() end
 
 ---Plays the click sound (the one in minecraft when u clicc button)
----@return nil
+---Will not work on 1.18.30+
 function gui.clickSound() end
 
 ---Plays a sound on the ui (will not go away if you move)
----@param name string [any minecraft sounds](https://www.digminecraft.com/lists/sound_list_pe.php)
----@return nil
+---Will not work on 1.18.30+
+---@param name string any minecraft sounds: https://www.digminecraft.com/lists/sound_list_pe.php
 function gui.sound(name) end
 
 ---Stops all sound that are playing
----@return nil
+---Will not work on 1.18.30+
 function gui.stopallsound() end
 
----Stops all sound that are playing
----@return nil
-function gui.stopallsound() end
 
----Stops all sound that are playing
----@return Theme
+---Gets the user's theme
+---@return Theme theme The user's theme
 function gui.theme() end
 
----Stops all sound that are playing
----@return Font
+---Gets the font in use right now
+---@return Font font The current font
 function gui.font() end
 
 
@@ -95,13 +95,13 @@ local _acp_font = {}
 
 ---Returns the size of the text
 ---@param text string The text to get the size of
----@return number widthOfText
+---@return number widthOfText The width of the input text
 ---@diagnostic disable-next-line: duplicate-set-field
 function _acp_font.width(text) end
 
 ---Returns the size of the text
 ---@param text string The text to get the size of
 ---@param scale number The scale
----@return number widthOfText
+---@return number widthOfText The width of the input text
 ---@diagnostic disable-next-line: duplicate-set-field
 function _acp_font.width(text, scale) end
