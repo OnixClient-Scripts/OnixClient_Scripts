160a3131fd41c5f75e8b45b88322ad4a5f391ff7
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2023-01-06 10:34:29 -0500

    Update events.lua

--- AutoComplete/library/events.lua
+++ AutoComplete/library/events.lua
@@ -71,7 +71,7 @@ event = {}
 ---event.listen("LocalServerUpdate", function()
 ---    
 ---end)
----@param eventName string | '"KeyboardInput", function(key, down)\n\t\nend' | '"MouseInput", function(button, down)\n\t\nend' | '"ChatMessageAdded", function(message, username, type, xuid)\n\t\nend' | '"LocalDataReceived", function(uuid, content)\n\t\nend' | '"ConfigurationSaved", function()\n\tlocal data = {}\n\t\n\treturn data\nend' | '"ConfigurationLoaded", function(data)\n\t\nend' | '"LocalServerUpdate", function()\n\t\nend' Name of the event to listen to
+---@param eventName string | '"KeyboardInput", function(key, down)\n\t\nend' | '"MouseInput", function(button, down)\n\t\nend' | '"ChatMessageAdded", function(message, username, type, xuid)\n\t\nend' | '"LocalDataReceived", function(uuid, content)\n\t\nend' | '"ConfigurationSaved", function()\n\tlocal data = {}\n\t\n\treturn data\nend' | '"ConfigurationLoaded", function(data)\n\t\nend' | '"LocalServerUpdate", function()\n\t\nend' | '"BlockChanged", function(x, y, z, newBlock, oldBlock)\n\t\nend' Name of the event to listen to
 ---@param handler function Function that will handle the event
 ---@return nil
 function event.listen(eventName, handler) end
