a6d30113889108d780d7e7510bddd2c47229100d
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2023-03-22 11:28:39 -0400

    Update server.lua

--- AutoComplete/library/server.lua
+++ AutoComplete/library/server.lua
@@ -28,3 +28,39 @@ function server.worldName() end
 ---end
 ---@return string[] playernames The name of everybody in the playerlist (same as pause menu)
 function server.players() end
+
+
+---@class Objective
+---@field name string The name of the objective
+---@field displayName string The display/pretty name of the objective
+---@field scores table The scores, keys are the name of the holder and the value is the score
+
+---@class DisplayObjective : Objective
+---@field isDescendingOrder boolean If the objective is sorted in descending order (big boy first)
+
+---@class Scoreboard
+local _acp__Scoreboard_z = {}
+---Gives you the display objective for that location
+---@param place string|"sidebar"|"belowname"|"list"
+---@return DisplayObjective objective The display objective
+function _acp__Scoreboard_z.getDisplayObjective(place) end
+
+---Gives you the display objective for that location
+---Note: that its not it's not guaranteed you will get anything beyond the display ones.
+---@param name string The name of the objective (not the display name)
+---@return Objective|nil objective The objective or nil if it was not found
+function _acp__Scoreboard_z.getObjective(name) end
+
+---Gives you the display objective for that location
+---Note: that its not it's not guaranteed you will get anything beyond the display ones.
+---@return Objective[] objectives The objectives
+function _acp__Scoreboard_z.getObjectives() end
+
+---Gives you the display objective for that location
+---Note: that its not it's not guaranteed you will get anything beyond the display ones.
+---@return string[] objectives The objective names
+function _acp__Scoreboard_z.getObjectiveNames() end
+
+--Gets you the server scoreboard
+---@return Scoreboard scoreboard The scoreboard
+function server.scoreboard() end
