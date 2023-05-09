f98b4cdea3209d3f74162726fa8922d21fe356fc
Zeuroux <127013771+Zeuroux@users.noreply.github.com>
2023-05-09 23:52:06 +0800

    Update Sudoku.lua

--- Modules/Sudoku.lua
+++ Modules/Sudoku.lua
@@ -5,63 +5,96 @@ positionX = 0
 positionY = 0
 sizeX = 100
 sizeY = 100
-
+difficulties = { { 1, "Easy" }, { 2, "Medium" }, { 3, "Hard" }, { 4, "Random" } }
 show_mouse_key = client.settings.addNamelessKeybind("Show Mouse", 220)
-puz_grid = {}
-for i = 0, 8 do
-    puz_grid[i] = {}
-    for j = 0, 8 do
-        puz_grid[i][j] = ""
-    end
-end
-lives = 3
+--n key
+new_game_key = client.settings.addNamelessKeybind("New Game", 49)
+difficulty = client.settings.addNamelessEnum("Difficulty", 1, difficulties)
+loading_text = "Loading..."
+wrong = {}
+
 function render2()
     gfx2.color(255, 255, 255, 255)
     gfx2.fillRect(0, 0, sizeX, sizeY)
-    for i = 0, 8 do
-        for j = 0, 8 do
-            gfx2.color(136, 136, 136)
-            gfx2.drawRect(i * 11.1, j * 11.1, 11.1, 11.1, 0.5)
-            gfx2.color(0, 0, 0)
-            if original_grid[i][j] ~= "" then
-                gfx2.color(38, 97, 156)
-            end
-            gfx2.text(i * 11.1 + 3.5, j * 11.1 + 1.5, puz_grid[i][j])
-        end
-    end
-    for i = 0, 8 do
-        for j = 0, 8 do
-            if i % 3 + j % 3 == 0 then
+    if doneLoading then
+        for i = 0, 8 do
+            for j = 0, 8 do
+                gfx2.color(136, 136, 136)
+                gfx2.drawRect(i * 11.1, j * 11.1, 11.1, 11.1, 0.5)
                 gfx2.color(0, 0, 0)
-                gfx2.drawRect(i * 11.1, j * 11.1, 33.3, 33.3, 0.5)
-            end
-            if selected ~= nil and selected[1] == i and selected[2] == j then
-                gfx2.color(0, 255, 0)
-                gfx2.drawRect(i * 11.1, j * 11.1, 11.1, 11.1, 1)
+                if original_grid[i][j] ~= "" then
+                    gfx2.color(38, 97, 156)
+                end
+                for k = 1, #wrong do
+                    if wrong[k][1] == i and wrong[k][2] == j then
+                        gfx2.color(255, 0, 0)
+                    end
+                end
+                gfx2.text(i * 11.1 + 3.5, j * 11.1 + 1.5, puz_grid[i][j])
             end
         end
+        for i = 0, 8 do
+            for j = 0, 8 do
+                if i % 3 + j % 3 == 0 then
+                    gfx2.color(0, 0, 0)
+                    gfx2.drawRect(i * 11.1, j * 11.1, 33.3, 33.3, 0.5)
+                end
+                if selected ~= nil and selected[1] == i and selected[2] == j then
+                    gfx2.color(0, 255, 0)
+                    gfx2.drawRect(i * 11.1, j * 11.1, 11.1, 11.1, 1)
+                end
+            end
+        end
+    else
+        gfx2.color(0, 0, 0)
+        gfx2.text(37, 45, loading_text)
     end
 end
 
 event.listen("KeyboardInput", function(key, down)
+    if key == new_game_key.value and down then
+        newGame()
+        return true
+    end
     if key == show_mouse_key.value and down then
         gui.setGrab(not gui.mouseGrabbed())
         return true
     end
-    if key >= 49 and key <= 57 and down and selected ~= nil then
-        if isValid(puz_grid, selected[1], selected[2], key - 48) then
+    if gui.mouseGrabbed() == true and gui.screen() == "hud_screen" then
+        if key == 8 and down and selected ~= nil then
+            puz_grid[selected[1]][selected[2]] = ""
+        end
+        if key >= 49 and key <= 57 and down and selected ~= nil then
             puz_grid[selected[1]][selected[2]] = key - 48
-            if isSolved() then
-                print("You win!")
-                puz_grid = generateSudokuPuzzle()
+            if isValid(selected[1], selected[2], key - 48) then
+                if isSolved() then
+                    loading_text = "You win!"
+                    newGame()
+                end
+            else
+                table.insert(wrong, { selected[1], selected[2] })
+                lives = lives - 1
+                if lives == 0 then
+                    loading_text = "You lose!"
+                    newGame()
+                end
             end
-        else
-            print("Invalid move")
-            lives = lives - 1
-            if lives == 0 then
-                print("Game over")
-                puz_grid = generateSudokuPuzzle()
-                lives = 3
+            
+        end
+        return true
+    end
+end)
+
+event.listen("MouseInput", function(button, down)
+    if gui.mouseGrabbed() == true and gui.screen() == "hud_screen" then
+        if (button == 1 and down) then
+            local x, y = gui.mousex() - positionX, gui.mousey() - positionY
+            local x = math.floor(x / 11.1)
+            local y = math.floor(y / 11.1)
+            if x >= 0 and x <= 8 and y >= 0 and y <= 8 then
+                if original_grid[x][y] == "" then
+                    selected = { x, y }
+                end
             end
         end
         return true
@@ -79,96 +112,73 @@ function isSolved()
     return true
 end
 
-event.listen("MouseInput", function(button, down)
-    if (button == 1 and down and gui.mouseGrabbed() == true and gui.screen() == "hud_screen") then
-        local x, y = gui.mousex() - positionX, gui.mousey() - positionY
-        local x = math.floor(x / 11.1)
-        local y = math.floor(y / 11.1)
-        if x >= 0 and x <= 8 and y >= 0 and y <= 8 then
-            if original_grid[x][y] == "" then
-                selected = { x, y }
-            end
-        end
-        return true
-    end
-end)
-
 function postInit()
-    puz_grid = generateSudokuPuzzle()
+    newGame()
 end
 
-function isValid(board, row, column, num)
-    local pos = { row, column }
-    for i = 0, 8 do
-        if board[i][pos[2]] == num and { i, pos[2] } ~= pos then
-            return false
-        end
+function newGame()
+    if difficulty.value == 1 then
+        diff = "easy"
+    elseif difficulty.value == 2 then
+        diff = "medium"
+    elseif difficulty.value == 3 then
+        diff = "hard"
+    else
+        diff = "random"
     end
-    for j = 0, 8 do
-        if board[pos[1]][j] == num and { pos[1], j } ~= pos then
-            return false
-        end
-    end
-
-    local start_i = pos[1] - (pos[1] % 3)
-    local start_j = pos[2] - (pos[2] % 3)
-
-    for i = 0, 2 do
-        for j = 0, 2 do
-            if board[start_i + i][start_j + j] == num and { start_i + i, start_j + j } ~= pos then
-                return false
-            end
-        end
-    end
-
-    return true
+    url = "https://sugoku.onrender.com/board?difficulty=" .. diff
+    network.get(url, "Sudoku")
+    lives = 3
+    doneLoading = false
 end
 
-function solveSudoku(grid)
-    for row = 0, 8 do
-        for col = 0, 8 do
-            if grid[row][col] == "" then
-                for num = 1, 9 do
-                    if isValid(grid, row, col, num) then
-                        grid[row][col] = num
-                        if solveSudoku(grid) then
-                            return true
-                        end
-                        grid[row][col] = ""
-                    end
+function onNetworkData(code, id, data)
+    if id == "Sudoku" then
+        jsonString = data
+        network.post("https://sugoku.onrender.com/solve", "Sudoku_solved", data)
+        puz_grid = generateSudokuPuzzle()
+    end
+    if id == "Sudoku_solved" then
+        solved_board = jsonToTable(data).solution
+        local grid = {}
+        for i = 0, 8 do
+            grid[i] = {}
+            for j = 0, 8 do
+                if solved_board[i + 1][j + 1] == 0 then
+                    grid[i][j] = ""
+                else
+                    grid[i][j] = solved_board[i + 1][j + 1]
                 end
-                return false
             end
         end
+        solved_grid = grid
+        doneLoading = true
     end
-
-    return true
 end
 
 function generateSudokuPuzzle()
+    local data = jsonToTable(jsonString)
+    local boardArray = data.board
     local grid = {}
     for i = 0, 8 do
         grid[i] = {}
         for j = 0, 8 do
-            grid[i][j] = ""
-        end
-    end
-
-    solveSudoku(grid)
-
-    local numDigitsToRemove = math.random(40, 50)
-
-    while numDigitsToRemove > 0 do
-        local row = math.random(0, 8)
-        local col = math.random(0, 8)
-
-        if grid[row][col] ~= "" then
-            grid[row][col] = ""
-            numDigitsToRemove = numDigitsToRemove - 1
+            if boardArray[i + 1][j + 1] == 0 then
+                grid[i][j] = ""
+            else
+                grid[i][j] = boardArray[i + 1][j + 1]
+            end
         end
     end
     original_grid = deepCopy(grid)
     return grid
+end
+
+function isValid(row, col, num)
+    if solved_grid[row][col] ~= num then
+        return false
+    end
+    return true
 end
 
 function deepCopy(orig)
