9d2c2ee0ca8800ffb112178142ab5e33fe6ea0e9
bowtie cat <65556454+jqms@users.noreply.github.com>
2023-05-24 00:13:42 +0100

    copies hive cs codes

---
+++ Modules/HiveCSCodeCopier.lua
@@ -0,0 +1,25 @@
+name = "Hive CS Code Copier"
+description = "Copies the custom server code from the Hive to your clipboard."
+
+showChatMessage = client.settings.addNamelessBool("Show Chat Message", true)
+
+copied = false
+
+event.listen("TitleChanged", function(text, titleType)
+    if text:find("§eJoin Code: ") then
+        local code = text:match("§eJoin Code: §.(.*)")
+        setClipboard(code)
+        if showChatMessage.value then
+            if copied == false then
+                print("§a§l» §rCopied code §e" .. code .. "§f to clipboard!§r")
+                copied = true
+            end
+        end
+    end
+end)
+
+event.listen("ChatMessageAdded", function(message)
+    if message:match(" joined. §.") and message:find(player.name()) then
+        copied = false
+    end
+end)
