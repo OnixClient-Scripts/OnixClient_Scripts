219afa46afd2a6af86eee20aedca748715963d7f
bowtie cat <65556454+jqms@users.noreply.github.com>
2023-05-25 16:57:13 +0100

    now uses sounddownloader lib

--- Modules/KillDing.lua
+++ Modules/KillDing.lua
@@ -1,18 +1,12 @@
 name = "Kill Ding"
 description = "Plays a ding sound when you kill someone on The Hive."
 
-function postInit()
-    if not fs.exist("KillDing/ding.mp3") then
-        fs.mkdir("KillDing")
-        network.fileget("KillDing/ding.mp3","https://github.com/OnixClient-Scripts/OnixClient_Scripts/raw/master/Data/KillDing/ding.mp3", "ding")
-    end
-end
+importLib("SoundDownloader")
 
--- this is only here because it needs to be lol
-function onNetworkData(code,id,data)
-    if id == "ding" then
-        return
-    end
+workingDir = "RoamingState/OnixClient/Scripts/Data/Sounds/"
+
+function postInit()
+    downloadSound("https://github.com/OnixClient-Scripts/OnixClient_Scripts/raw/master/Data/Sounds/ding.mp3")
 end
 
 event.listen("ChatMessageAdded", function(message, username, type, xuid)
@@ -22,7 +16,7 @@ event.listen("ChatMessageAdded", function(message, username, type, xuid)
     }
     for i,v in pairs(killMessages) do
         if message:match(v) then
-            playCustomSound("KillDing/ding.mp3")
+            playCustomSound("ding.mp3")
         end
     end
 end)
