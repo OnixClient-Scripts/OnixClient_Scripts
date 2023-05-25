0a2aeb8635d78f3cc76212cd9e3319a62606ab29
bowtie cat <65556454+jqms@users.noreply.github.com>
2023-05-25 16:53:10 +0100

    usage: downloadSound(url)

---
+++ Libs/SoundDownloader.lua
@@ -0,0 +1,24 @@
+workingDir = "RoamingState/OnixClient/Scripts/Data/Sounds/"
+
+---Downloads a sound from the OnixClient_Scripts repo (upload sounds to the sound folder in data on the repo)
+---@param url string The url of the sound to download
+---@return nil
+function downloadSound(url)
+    workingDir = "RoamingState/OnixClient/Scripts/Data"
+    if not fs.exist("Sounds") then
+        fs.mkdir("Sounds")
+    end
+    workingDir = "RoamingState/OnixClient/Scripts/Data/Sounds/"
+    local name = url:match("([^/]+)$")
+
+    if not fs.exist(name) then
+        network.fileget(name,url, name)
+    end
+end
+
+-- this is only here because it has to be lol
+function onNetworkData(code,id,data)
+    if id then
+        return
+    end
+end
