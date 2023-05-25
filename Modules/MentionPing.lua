585805f8787194acbc43c3d361c2a65019ebcbba
bowtie cat <65556454+jqms@users.noreply.github.com>
2023-05-25 16:57:02 +0100

    now uses sounddownloader lib

--- Modules/MentionPing.lua
+++ Modules/MentionPing.lua
@@ -1,30 +1,16 @@
 name = "Mention Ping"
 description = "Makes it so when you get pinged in chat, it will play a sound."
 
+importLib("SoundDownloader")
+
 workingDir = "RoamingState/OnixClient/Scripts/Data/Sounds/"
 
 discordMode = client.settings.addNamelessBool("Use Discord Ping", false)
 respondToHere = client.settings.addNamelessBool("Respond to @here", false)
 
 function postInit()
-    workingDir = "RoamingState/OnixClient/Scripts/Data"
-    if not fs.exist("Sounds") then
-        fs.mkdir("Sounds")
-    end
-    workingDir = "RoamingState/OnixClient/Scripts/Data/Sounds/"
-    if not fs.exist("ding.mp3") then
-        network.fileget("ding.mp3","https://github.com/OnixClient-Scripts/OnixClient_Scripts/raw/master/Data/Sounds/ding.mp3", "ding")
-    end
-    if not fs.exist("discord.mp3") then
-        network.fileget("discord.mp3","https://github.com/OnixClient-Scripts/OnixClient_Scripts/raw/master/Data/Sounds/discord.mp3", "discord")
-    end
-end
-
--- this is only here because it has to be lol
-function onNetworkData(code,id,data)
-    if id == "ding" or id == "discord" then
-        return
-    end
+    downloadSound("https://github.com/OnixClient-Scripts/OnixClient_Scripts/raw/master/Data/Sounds/ding.mp3")
+    downloadSound("https://github.com/OnixClient-Scripts/OnixClient_Scripts/raw/master/Data/Sounds/discord.mp3")
 end
 
 shouldPing = false
@@ -47,5 +33,4 @@ event.listen("ChatMessageAdded", function(message, username, type, xuid)
         end
         shouldPing = false
     end
-    return
 end)
