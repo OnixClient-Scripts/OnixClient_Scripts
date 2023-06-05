06ec80a2922b04137db93661e26feda87065e5bd
jqms <65556454+jqms@users.noreply.github.com>
2023-06-05 22:38:27 +0100

    now supports tables and tells you when text is nil

--- Libs/OnixClientVSLogger.lua
+++ Libs/OnixClientVSLogger.lua
@@ -1,12 +1,23 @@
 -- for use with https://github.com/jqms/OnixClientLogger
 vs = {
+---Logs a string to the OnixVSLogs.txt file
+---@param text string The text to log
+---@return nil
     log = function (text)
         local path = "OnixVSLogs/OnixVSLogs.txt"
         local file = io.open(path, "a")
+        if type(text) == "table" then
+            text = tableToJson(text)
+        end
+        if text == nil then
+            text = "nil"
+        end
         file:write(text .. "\n")
         file:close()
     end,
 
+---Clears the OnixVSLogs.txt file
+---@return nil
     clearLog = function ()
         if fs.exist("OnixVSLogs/OnixVSLogs.txt") then
             fs.delete("OnixVSLogs/OnixVSLogs.txt")
