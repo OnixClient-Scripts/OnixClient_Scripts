e64472ccb7c78418d3522d694530fb0d1fce1c4d
jqms <65556454+jqms@users.noreply.github.com>
2023-06-05 23:35:37 +0100

    Update OnixClientVSLogger.lua

--- Libs/OnixClientVSLogger.lua
+++ Libs/OnixClientVSLogger.lua
@@ -1,4 +1,39 @@
 -- for use with https://github.com/jqms/OnixClientLogger
+
+function getHash()
+    workingDir = "RoamingState/OnixClient/Scripts/Libs/"
+    hash = fs.hash("OnixClientVSLogger.lua")
+    network.get("https://raw.githubusercontent.com/OnixClient-Scripts/OnixClient_Scripts/master/index.json", "index")
+    workingDir = "RoamingState/OnixClient/Scripts/Data"
+end
+
+function onNetworkData(code,id,data)
+    if id == "index" then
+        local index = jsonToTable(data)
+        if index["libs"] then
+            for i,lib in pairs(index["libs"]) do
+                if lib["name"] == "OnixClientVSLogger" then
+                    if lib["hash"] == hash then
+                        network.get(lib["url"], "lib")
+                    else
+                        vs.log("OnixClientVSLogger hash mismatch... Updating...")
+                        network.get(lib["url"], "lib")
+                    end
+                end
+            end
+        end
+    end
+    if id == "lib" then
+        workingDir = "RoamingState/OnixClient/Scripts/Libs/"
+        io.open("OnixClientVSLogger.lua", "w"):write(data):close()
+        vs.log("OnixClientVSLogger updated!")
+    end
+end
+
+function postInit()
+    getHash()
+end
+
 vs = {
 ---Logs a string to the OnixVSLogs.txt file
 ---@param text string The text to log
@@ -8,6 +43,13 @@ vs = {
         local file = io.open(path, "a")
         if type(text) == "table" then
             text = tableToJson(text)
+        end
+        if type(text) == "boolean" then
+            if text then
+                text = "true"
+            else
+                text = "false"
+            end
         end
         if text == nil then
             text = "nil"
