8487820f0ddb79550461badc61253a64660023df
CreeperG16 <pappt1216@gmail.com>
2023-08-14 18:15:18 +0200

    Add last updated to indexer

--- Extras/indexer/indexer.js
+++ Extras/indexer/indexer.js
@@ -11,11 +11,15 @@ if (!process.argv[0].endsWith("node") && !process.argv[0].endsWith("node.exe"))
 
 console.log("DIRNAME:", __dirname);
 
+const oldIndex = require("../../index.json");
+
 (async () => {
   //modules
   const modules = await readdir(path.join(__dirname, "../../Modules"));
   const moduleIndex = await Promise.all(
     modules.map(async (moduleFile) => {
+      const oldModule = oldIndex.modules.find((x) => x.file === moduleFile);
+
       const moduleContent = await readFile(path.join(__dirname, "../../Modules", moduleFile)).then((x) => x.toString());
       const moduleName = moduleContent.match(/name( *?)=( *?)(.+?)(\r|\n)/g)[0].match(/(('|")(.+?)('|"))/)[3];
       const moduleDesc = moduleContent.match(/description( *?)=( *?)(.+?)(\r|\n)/g)[0].match(/(('|")(.+?)('|"))/)?.[3];
@@ -27,6 +31,7 @@ console.log("DIRNAME:", __dirname);
         moduleContent.match(/registerCommand( *?)\(('|")(.+?)('|")/g)?.map?.((x) => x.match(/(('|")(.+?)('|"))/)[3]) ??
         [];
       const moduleHash = crypto.createHash("md5").update(moduleContent).digest().toString("hex");
+      const moduleLastUpdated = oldModule?.hash === moduleHash ? oldModule?.lastUpdated ?? new Date() : new Date();
       return {
         name: moduleName,
         file: moduleFile,
@@ -35,6 +40,7 @@ console.log("DIRNAME:", __dirname);
         libs: moduleDeps,
         commands: moduleCommands,
         hash: moduleHash,
+        lastUpdated: moduleLastUpdated,
       };
     })
   );
@@ -43,6 +49,8 @@ console.log("DIRNAME:", __dirname);
   const commands = await readdir(path.join(__dirname, "../../Commands"));
   const commandIndex = await Promise.all(
     commands.map(async (commandFile) => {
+      const oldCommand = oldIndex.commands.find((x) => x.file === commandFile);
+
       const commandContent = await readFile(path.join(__dirname, "../../Commands", commandFile)).then((x) =>
         x.toString()
       );
@@ -55,6 +63,7 @@ console.log("DIRNAME:", __dirname);
           .match(/importLib( *?)\(( *?)("|')(.+?)("|')\)/g)
           ?.map?.((x) => x.match(/(('|")(.+?)('|"))/)[3].replace(/\.lua$/, "")) ?? [];
       const commandHash = crypto.createHash("md5").update(commandContent).digest().toString("hex");
+      const commandLastUpdated = oldCommand?.hash === commandHash ? oldCommand?.lastUpdated ?? new Date() : new Date();
       return {
         name: commandName,
         file: commandFile,
@@ -62,6 +71,7 @@ console.log("DIRNAME:", __dirname);
         url: "https://raw.githubusercontent.com/OnixClient-Scripts/OnixClient_Scripts/master/Commands/" + commandFile,
         libs: commandDeps,
         hash: commandHash,
+        lastUpdated: commandLastUpdated,
       };
     })
   );
@@ -70,38 +80,52 @@ console.log("DIRNAME:", __dirname);
   const libs = await readdir(path.join(__dirname, "../../Libs"));
   const libIndex = await Promise.all(
     libs.map(async (libFile) => {
+      const oldLib = oldIndex.libs.find((x) => x.file === libFile);
       const libContent = await readFile(path.join(__dirname, "../../Libs", libFile));
       const libHash = crypto.createHash("md5").update(libContent).digest().toString("hex");
+      const libLastUpdated = oldLib?.hash === libHash ? oldLib?.lastUpdated ?? new Date() : new Date();
       return {
         name: libFile.replace(/\.lua$/, ""),
         file: libFile,
         url: "https://raw.githubusercontent.com/OnixClient-Scripts/OnixClient_Scripts/master/Libs/" + libFile,
         hash: libHash,
+        lastUpdated: libLastUpdated,
       };
     })
   );
 
   //autocomplete
+  const acConfigHash = crypto
+    .createHash("md5")
+    .update(await readFile(path.join(__dirname, "../../AutoComplete/config.lua")))
+    .digest()
+    .toString("hex");
+
+  const acConfigLastUpdated =
+    oldIndex.autocomplete.config.hash === acConfigHash
+      ? oldIndex.autocomplete.config.lastUpdated ?? new Date()
+      : new Date();
+
   const autocompleteConfig = {
     file: "config.lua",
     url: "https://raw.githubusercontent.com/OnixClient-Scripts/OnixClient_Scripts/master/AutoComplete/config.lua",
-    hash: crypto
-      .createHash("md5")
-      .update(await readFile(path.join(__dirname, "../../AutoComplete/config.lua")))
-      .digest()
-      .toString("hex"),
+    hash: acConfigHash,
+    lastUpdated: acConfigLastUpdated,
   };
   const autocomplete = await readdir(path.join(__dirname, "../../AutoComplete/library"));
   const autocompleteIndex = await Promise.all(
     autocomplete.map(async (acFile) => {
+      const oldACFile = oldIndex.autocomplete.library.find((x) => x.file === acFile);
       const acContent = await readFile(path.join(__dirname, "../../AutoComplete/library", acFile));
       const acHash = crypto.createHash("md5").update(acContent).digest().toString("hex");
+      const acLastUpdated = oldACFile?.hash === acHash ? oldACFile?.lastUpdated ?? new Date() : new Date();
       return {
         file: acFile,
         url:
           "https://raw.githubusercontent.com/OnixClient-Scripts/OnixClient_Scripts/master/AutoComplete/library" +
           acFile,
         hash: acHash,
+        lastUpdated: acLastUpdated,
       };
     })
   );
