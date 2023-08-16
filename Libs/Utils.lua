f9e82cf3d597aa08a10866ba6bce04c0e17cdee7
jqms <65556454+jqms@users.noreply.github.com>
2023-08-16 21:26:23 +0100

    Add files via upload

---
+++ Libs/Utils.lua
@@ -0,0 +1,28 @@
+ui = {
+    --- Gets a setting from a module
+    --- @param mod string The module name
+    --- @param settingName string The setting name
+    --- @return table settingsTable The setting table
+    getSettingFromMod = function(mod,settingName)
+        local modules = client.modules()
+        for i,v in pairs(modules) do
+            if v.name == mod then
+                for _,k in pairs(v.settings) do
+                    if k.name == settingName then
+                        return k
+                    end
+                end
+            end
+        end
+        return {}
+    end,
+    getModByName = function(mod)
+        local modules = client.modules()
+        for i,v in pairs(modules) do
+            if v.name == mod then
+                return v
+            end
+        end
+        return {}
+    end,
+}
