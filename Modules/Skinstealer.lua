1483df8bb81778f6995d5637d558c652b1bbd336
jqms <65556454+jqms@users.noreply.github.com>
2023-08-17 03:08:36 +0100

    added skin clone. USE LATEST TEST IF IT DOESNT WORK!

--- Modules/Skinstealer.lua
+++ Modules/Skinstealer.lua
@@ -1,5 +1,5 @@
 name="Skinstealer"
-description="Steals peoples skins"
+description="Steals peoples skins."
 
 function stealMySkin()
     local mySkin = player.skin()
@@ -45,33 +45,43 @@ fs.mkdir("Skinstealer")
 grabSkin = false
 username = ""
 usernameTest = ""
-skinstealKey = client.settings.addNamelessKeybind("Skinsteal Key", 0)
-shutUpBloat = client.settings.addNamelessBool("Hide all settings",false)
+
+
 client.settings.addAir(5)
-mouseSettingsInfo = client.settings.addTitle("Mouse Settings")
-mouseSettingsInfo.scale = 1.4
+client.settings.addCategory("Skinsteal Settings")
+skinstealKey = client.settings.addNamelessKeybind("Skinsteal Key", 0)
+client.settings.addAir(5)
+
+client.settings.addCategory("Mouse Settings")
 disableMiddleClick = client.settings.addNamelessBool("Disable Mouse Button Skin Stealing", false)
 disableMouseButtons = client.settings.addNamelessBool("Prohibit Selected Mouse Button Input", false)
 mouseButtonToUse = client.settings.addNamelessInt("Mouse Button to Use",1,3,3)
 mouseButtonInfo = "Mouse Button: Middle"
 client.settings.addInfo("mouseButtonInfo").scale = 0.9
 mouseSettingsAir = client.settings.addAir(5)
-miscSettingsInfo = client.settings.addTitle("Misc Settings")
-miscSettingsInfo.scale = 1.4
+
+client.settings.addCategory("Clone Settings")
+cloneSkin = client.settings.addNamelessBool("Clone Skin", false)
+dontStealSkinWhenCloning = client.settings.addNamelessBool("Don't Steal Skin When Cloning", false)
+client.settings.addAir(5)
+
+client.settings.addCategory("Misc Settings")
 holdToSteal = client.settings.addNamelessBool("Hold to steal skins", false)
 skinStealFolderFunc = client.settings.addFunction("Open Skinsteal Folder","openSkinstealFolder","Open")
 lastSkinFolderFunc = client.settings.addFunction("Open Last Stolen Skin Folder","openLastSkin","Open")
 stealLocalSkin = client.settings.addFunction("Steal Your Own Skin", "stealMySkin", "Steal")
 client.settings.addAir(5)
-chatSettingsInfo = client.settings.addTitle("Chat Settings")
-chatSettingsInfo.scale = 1.4
+
+client.settings.addCategory("Chat Settings")
 disableChatMessage = client.settings.addNamelessBool("Disable Chat Messages",false)
 warningMessage = client.settings.addNamelessBool("Disable \"Could not steal skin.\" message.",false)
+
 local mouseButtons = {
     "Mouse Button: Left",
     "Mouse Button: Right",
     "Mouse Button: Middle"
 }
+
 function setDescriptionScale(scale)
     local settings = holdToSteal.parent.settings
     for _, s in pairs(settings) do
@@ -81,45 +91,17 @@ function setDescriptionScale(scale)
         end
     end
 end
-function update()
-    if shutUpBloat.value then
-        mouseButtonToUse.visible = false
-        mouseSettingsAir.visible = false
-        mouseSettingsInfo.visible = false
-        disableMiddleClick.visible = false
-        holdToSteal.visible = false
-        disableChatMessage.visible = false
-        warningMessage.visible = false
-        disableMouseButtons.visible = false
-        chatSettingsInfo.visible = false
-        miscSettingsInfo.visible = false
-        skinStealFolderFunc.visible = false
-        lastSkinFolderFunc.visible = false
-        stealLocalSkin.visible = false
-        mouseButtonInfo = ""
 
-    else
-        mouseButtonToUse.visible = true
-        mouseSettingsAir.visible = true
-        mouseSettingsInfo.visible = true
-        disableMiddleClick.visible = true
-        holdToSteal.visible = true
-        disableChatMessage.visible = true
-        disableMouseButtons.visible = true
-        warningMessage.visible = true
-        chatSettingsInfo.visible = true
-        miscSettingsInfo.visible = true
-        skinStealFolderFunc.visible = true
-        lastSkinFolderFunc.visible = true
-        stealLocalSkin.visible = true
-    end
+function cloneSkinFunc()
+    local selectedSkin = player.selectedEntity().skin()
+    local skin = player.skin()
+
+    skin.setSkinCapeGeometry(selectedSkin.texture(), selectedSkin.capeTexture(), selectedSkin.geometry(), selectedSkin.defaultGeometryName(), selectedSkin.geometryName())
+end
+
+function update()
     setDescriptionScale(1.25)
-    if disableMiddleClick.value then
-        disableMouseButtons.visible = false
-        mouseButtonToUse.visible = false
-        mouseSettingsAir.visible = false
-        mouseButtonInfo = ""
-    elseif disableMiddleClick.value == false and shutUpBloat.value == false then
+    if disableMiddleClick.value == false then
         disableMouseButtons.visible = true
         mouseButtonToUse.visible = true
         mouseSettingsAir.visible = true
@@ -132,7 +114,7 @@ function update()
         skinsteal()
     end
 end
-event.listen("KeyboardInput", function(key,down) 
+event.listen("KeyboardInput", function(key,down)
     if gui.mouseGrabbed() == true then return end
     if key == skinstealKey.value and down then
         skinsteal()
@@ -141,7 +123,7 @@ event.listen("KeyboardInput", function(key,down)
         holdSteal = down
     end
 end)
-event.listen("MouseInput", function(button,down) 
+event.listen("MouseInput", function(button,down)
     if gui.mouseGrabbed() == true then return end
     if button == mouseButtonToUse.value and down and disableMiddleClick.value == false then
         skinsteal()
@@ -163,6 +145,10 @@ function skinsteal()
                 return
             end
         else
+            if cloneSkin.value then
+                cloneSkinFunc()
+            end
+            if dontStealSkinWhenCloning.value then return end
             local selectedPlayer = player.selectedEntity()
             if selectedPlayer.username ~= nil then
                 local playerName = string.split(selectedPlayer.username, "\n")
@@ -209,7 +195,6 @@ registerCommand("skinsteal", function(args)
         print("§c§lSkinstealer Help§r\n§aArguments can be added with §r§o§7--[argument]§r.§a\n§lArgs:\n§r§7--lastStolen §a§oOpens the folder of the last stolen skin.")
     elseif args == "--showSettings" then
         print("§a")
-        shutUpBloat.value = false
     else
         print("§aUnknown argument \"§7" .. args .. "§a\".\ndo §o§7.skinsteal help §r§afor help")
     end
