1dad07437f9a088da32183b4240d1c39eb0bbd2a
jqms <65556454+jqms@users.noreply.github.com>
2023-08-18 22:47:48 +0100

    added hive support

--- Modules/BlockWarning.lua
+++ Modules/BlockWarning.lua
@@ -1,26 +1,27 @@
 name = "Block Warning"
-description = "Displays a warning when you are low on blocks"
+description = "Displays a warning when you are low on blocks."
 
-positionX = 50
-positionY = 50
 sizeX = 24
 sizeY = 24
+positionX = 50
+positionY = 50
 
-client.settings.addTitle("Warning Settings")
 client.settings.addAir(5)
+client.settings.addCategory("Warning Settings")
 client.settings.addInfo("The warning will be triggered when you have the specified amount of blocks left")
 warning = client.settings.addNamelessInt("Blocks Left:", 1, 64, 10)
+client.settings.addAir(5)
 
 function render(dt)
     local inventory = player.inventory()
     local slot = inventory.selected
     local item = inventory.at(slot)
-    
+
     if item ~= nil then
         local count = item.count
         local itemtype = item.name
-        
-        if string.find(itemtype, "wool") then
+        local serverIP = server.ipConnected()
+        if itemtype:find("wool") or (itemtype:find("stone") and serverIP:find("hive")) then
             if count <= warning.value then
                 gfx.color(255, 18, 18)
                 gfx.text(0, 0, "⚠", 3)
