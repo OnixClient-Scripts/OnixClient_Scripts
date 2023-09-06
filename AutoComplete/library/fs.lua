cf501659e1c6e2114dcd12f589cd3f300e6c1030
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2023-09-06 13:02:19 -0400

    Update fs.lua

--- AutoComplete/library/fs.lua
+++ AutoComplete/library/fs.lua
@@ -94,6 +94,10 @@ function _acp_BinaryFile:eof() end
 ---@return integer fileSize The size of the file
 function _acp_BinaryFile:size() end
 
+---The amount of bytes availible forward from the current position
+---@return integer availible The size of the availible bytes forward from the current position
+function _acp_BinaryFile:availible() end
+
 ---Goes to a position in the file
 ---@param position integer Where to go from the file start(0)
 function _acp_BinaryFile:seek(position) end
@@ -102,11 +106,25 @@ function _acp_BinaryFile:seek(position) end
 ---@return integer filepos Position in the file
 function _acp_BinaryFile:tell() end
 
+---Tells you if the current mode is little endian, x86 cpus are little endian and so its the default here too. Its included because some web/file formats require big endian.
+---Endianness is how multi-bytes numbers are read, for example 0x12345678 is 305419896 in decimal, in little endian it would be read as 0x78563412 and in big endian it would be read as 0x12345678
+---@return boolean isLittleEndian Position in the file
+function _acp_BinaryFile:isLittleEndian() end
+
+---Sets the endianness of the file
+---Endianness is how multi-bytes numbers are read, for example 0x12345678 is 305419896 in decimal, in little endian it would be read as 0x78563412 and in big endian it would be read as 0x12345678
+---@param isLittleEndian boolean If the file should be treated as is little endian
+function _acp_BinaryFile:setLittleEndian(isLittleEndian) end
+
 
 
 ---Writes content to the file, make sure not to screw up the byteCount
 ---@param content string What to take the bytes from
 ---@param byteCount integer how many bytes we taking from your string
+function _acp_BinaryFile:write(content, byteCount) end
+
+---Writes content to the file, make sure not to screw up the byteCount
+---@param content string What to take the bytes from, will take everything
 function _acp_BinaryFile:write(content, byteCount) end
 
 ---Writes a byte to the file
@@ -133,6 +151,10 @@ function _acp_BinaryFile:writeUShort(Short) end
 ---@param Int integer a number from from 0 to 4294967295
 function _acp_BinaryFile:writeUInt(Int) end
 
+---Writes an unsinged long to the file
+---@param Long integer a number from from 0 to 18,446,744,073,709,551,615
+function _acp_BinaryFile:writeULong(Long) end
+
 ---Writes a long to the file
 ---@param Long integer a number from from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
 function _acp_BinaryFile:writeLong(Long) end
@@ -146,13 +168,36 @@ function _acp_BinaryFile:writeFloat(Float) end
 ---@param Double number a number with decimal 
 function _acp_BinaryFile:writeDouble(Double) end
 
----Writes Text to the file
+---Writes text to the file (max 65535 characters)
 ---@param Text string the text to put in the file
 function _acp_BinaryFile:writeString(Text) end
+
+---Writes text to the file (max 4294967295 characters)
+---@param Text string the text to put in the file
+function _acp_BinaryFile:writeBigString(Text) end
 
 ---Writes Data to the file
 ---@param Data userdata the data to put in the file
 function _acp_BinaryFile:writeRaw(Data) end
+
+---Writes an entire stream to the file
+---@param Stream BinaryFile the data to put in the file
+function _acp_BinaryFile:writeStream(Stream) end
+
+---Writes part of a stream to the file
+---If the stream you write is compressed it will write the compressed data to this stream you can use :readRaw or :writeRaw to not compress it
+---@param Stream BinaryFile the data to put in the file
+---@param Begin integer|nil Where to start reading from the stream, nil will be 0 you can use :tell() to get the current position
+---@param End integer|nil Where to stop reading from the stream, nil will be the end of the stream
+function _acp_BinaryFile:writeStream(Stream, Begin, End) end
+
+---Writes part of a stream to the file
+---If the stream you write is compressed it will write the compressed data to this stream you can use :readRaw or :writeRaw to not compress it
+---@param Stream BinaryFile the data to put in the file
+---@param Begin integer|nil Where to start reading from the stream, nil will be 0 you can use :tell() to get the current position
+---@param End integer|nil Where to stop reading from the stream, nil will be the end of the stream
+function _acp_BinaryFile:writeStream(Stream, Begin, End) end
+
 
 
 
@@ -162,6 +207,10 @@ function _acp_BinaryFile:writeRaw(Data) end
 ---@param byteCount integer how many bytes we taking from the file
 ---@return string content What was read
 function _acp_BinaryFile:read(byteCount) end
+
+---Reads all content from the file
+---@return string content What was read
+function _acp_BinaryFile:read() end
 
 ---Reads a byte from the file
 ---@return integer Byte The byte that was read
@@ -191,6 +240,10 @@ function _acp_BinaryFile:readUInt() end
 ---@return integer Long The long that was read
 function _acp_BinaryFile:readLong() end
 
+---Reads a long from the file
+---@return integer Long The long that was read
+function _acp_BinaryFile:readULong() end
+
 ---Reads a float from the file
 ---@return number Float The float that was read
 function _acp_BinaryFile:readFloat() end
@@ -199,15 +252,32 @@ function _acp_BinaryFile:readFloat() end
 ---@return number Double The double that was read
 function _acp_BinaryFile:readDouble() end
 
----Reads Text from the file
+---Reads text from the file (max 65535 characters)
 ---@return string Text The text that was read
 function _acp_BinaryFile:readString() end
+
+---Reads text from the file (max 4294967295 characters)
+---@return string Text The text that was read
+function _acp_BinaryFile:readBigString() end
 
 ---Reads Data from the file
 ---@param bytesToRead integer How many bytes to read
 ---@return userdata Data The data that was read
 function _acp_BinaryFile:readRaw(bytesToRead) end
 
+---Reads a memory stream from the file
+---@param bytesToRead integer|nil How many bytes to read, nil would be everything ahead (:size() - :tell())
+---@return BinaryFile|nil Stream The stream that was read
+function _acp_BinaryFile:readStream(bytesToRead) end
+
+---Reads a memory stream from the file with potentially compressed data
+---@param bytesToRead integer|nil How many bytes to read, nil would be everything ahead (:size() - :tell())
+---@param compressionType string | "'lzma'" | "'deflate'" | "'gzip2'" | "'store'" | "'none'" The compression type to use, anything that isn't store or none will have compression enabled
+---@param compressionLevel integer | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 The compression level to use, 0 is terrible compression, 1 is fastest, 6 is balanced, 9 is best compression, if you get an output stream this will be its compression level.
+---@param compressionHeader boolean|nil If you want to skip the header, if you do you can't read the file without knowing the compression type
+---@param isOutputStream boolean|nil If you want to read the stream as an output stream, this will make it so you can write to the stream and it will write to it
+---@return BinaryFile|nil Stream The stream that was read
+function _acp_BinaryFile:readStream(bytesToRead, compressionType, compressionLevel, compressionHeader, isOutputStream) end
 
 
 ---Opens a file to read/write data
@@ -215,5 +285,33 @@ function _acp_BinaryFile:readRaw(bytesToRead) end
 ---@param openmode string | "'w'" | "'r'" | "'a'" how to open the file, w is write mode, r is read mode, a is append(and it will add to an existing file or start)
 ---@return BinaryFile|nil file The (hopefully) opened file
 function fs.open(path, openmode) end
+
+---Opens a memory stream to write data to (you can then write that stream to a file stream using :writeStream)
+---@return BinaryFile|nil file The (hopefully) opened file
+function fs.open() end
+
+---Opens a memory stream to write data to (you can then write that stream to a file stream using :writeStream)
+---@param path nil Leave nil to open a memory stream
+---@param openmode nil|string | "'w'" You can only write to this memory stream, you can create a "r" memory stream from it using :readStream
+---@return BinaryFile|nil file The (hopefully) opened file
+function fs.open(path, openmode) end
+
+---Opens a file to read/write data that can also be compressed
+---If you dont skip the header putting anything that isn't 'store' or 'none' will ignore what you supply and read the header, otherwise it will trust you
+---@param path string The path from Scripts/Data
+---@param openmode string | "'w'" | "'r'" | "'a'" how to open the file, w is write mode, r is read mode, a is append(and it will add to an existing file or start)
+---@param compressionType string | "'lzma'" | "'deflate'" | "'gzip2'" | "'store'" | "'none'" The compression type to use, anything that isn't store or none will have compression enabled
+---@param compressionLevel integer | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 The compression level to use, 0 is terrible compression, 1 is fastest, 6 is balanced, 9 is best compression
+---@param compressionHeader boolean | true | false If you want to skip the header, if you do you can't read the file without knowing the compression type
+function fs.open(path, openmode, compressionType, compressionLevel, compressionHeader) end
+
+---Opens a memory stream to write data that can also be compressed
+---If you dont skip the header putting anything that isn't 'store' or 'none' will ignore what you supply and read the header, otherwise it will trust you
+---@param path nil Leave nil to open a memory stream
+---@param openmode string | "'w'" | "'a'" how to open the file, w is write mode, a is append(and it will add to an existing file or start)
+---@param compressionType string | "'lzma'" | "'deflate'" | "'gzip2'" | "'store'" | "'none'" The compression type to use, anything that isn't store or none will have compression enabled
+---@param compressionLevel integer | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 The compression level to use, 0 is terrible compression, 1 is fastest, 6 is balanced, 9 is best compression
+---@param compressionHeader boolean | true | false If you want to skip the header, if you do you can't read the file without knowing the compression type
+function fs.open(path, openmode, compressionType, compressionLevel, compressionHeader) end
 
 
