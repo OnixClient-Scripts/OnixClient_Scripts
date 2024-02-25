93852d9faa9d7d052c510c67bc98975ce3618370
Jack Hirsh <39037393+jackhirsh@users.noreply.github.com>
2024-02-25 12:54:39 -0500

    Create mapLabeler.lua

---
+++ Modules/mapLabeler.lua
@@ -0,0 +1,28 @@
+name="Map Labeler"
+description="Labels Maps in game with their names"
+
+--[[
+    made by jackhirsh
+]]
+
+positionX = 35
+positionY = 120
+sizeX = 10
+sizeY = 10
+textColorSetting = client.settings.addNamelessColor("Text Color", {0, 0, 0})
+backgroundColorSetting = client.settings.addNamelessColor("Background Color", {255, 255, 255, 128})
+
+
+function render2(timeSinceUpdate)
+    inventory = player.inventory()
+    offHandItem = inventory.offhand()
+    offHandName = offHandItem.name
+    if (offHandName == "filled_map") then
+        local maxTextWidth = gfx2.textSize(offHandItem.displayName)
+        gfx2.color(backgroundColorSetting)
+        sizeX = maxTextWidth * 1.2
+        gfx2.fillRoundRect(0, 0, sizeX, sizeY, 2)
+        gfx2.color(textColorSetting)
+        gfx2.text(sizeX/10, 1, offHandItem.displayName)
+    end
+end
