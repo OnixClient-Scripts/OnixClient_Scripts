6e9c99b5a6a37b6e6801d465d3b5040293f3c036
WillDrac <101514340+O2Flash20@users.noreply.github.com>
2024-03-16 17:15:40 -0400

    small fixes

--- Modules/invenRender.lua
+++ Modules/invenRender.lua
@@ -8,10 +8,6 @@ Lock = false
 client.settings.addBool("Lock the position of the module?", "Lock")
 
 client.settings.addAir(8)
-bgColor = {0, 0, 0, 50}
-client.settings.addColor('Background Color?', 'bgColor')
-
-client.settings.addAir(8)
 column = 9
 client.settings.addInt("Number of columns", 'column', 1, 36)
 
@@ -23,18 +19,18 @@ positionX, positionY = gui.width() / 2 - 88, -13
 sizeX, sizeY, scale = 176, 67, 1
 
 function slotRender()
-    local originX, originY = 5, 17 --starting pos
-    local columnCurrent = math.floor(slot % column) --finds the remainder of the slot number / 9 
-    local rowCurrent = (math.floor(slot / column)) --finds the floored version of slot / 9
-    originY = rowCurrent * 18 --finds real row #
+    local originX, originY = 5, 17                  --starting pos
+    local columnCurrent = math.floor(slot % column) --finds the remainder of the slot number / 9
+    local rowCurrent = (math.floor(slot / column))  --finds the floored version of slot / 9
+    originY = rowCurrent * 18                       --finds real row #
     if columnCurrent == 0 then
-        columnCurrent = column --fixes there being no 9th slot
-        originY = originY - 18 --fixes an issue where it would render one slot below
+        columnCurrent = column                      --fixes there being no 9th slot
+        originY = originY - 18                      --fixes an issue where it would render one slot below
         sizeX = originX + 18 * columnCurrent + 4
     end
     originX = originX + 18 * columnCurrent - 18 --finds real coloumn #
-    originY = originY -3 -- alignment error fix
-    gfx.texture(originX, originY, 18, 18, 'textures/ui/item_cell.png')
+    originY = originY - 3                       -- alignment error fix
+    gfx.texture(originX, originY, 18, 18, 'textures/ui/item_cell')
     if slot == 36 then
         sizeY = originY + 20
     end
@@ -58,16 +54,16 @@ function render(dt)
     gfx.rect(0, 1, sizeX, sizeY)
 
     gfx.color(255, 255, 255)
-    gfx.rect(0, -1, sizeX, 2) --top highlight
-    gfx.rect(-2, 1, 2, sizeY -1) --left highlight
-    gfx.rect(-1, 0, 2, 2) --  left top corner
+    gfx.rect(0, -1, sizeX, 2)     --top highlight
+    gfx.rect(-2, 1, 2, sizeY - 1) --left highlight
+    gfx.rect(-1, 0, 2, 2)         --  left top corner
     gfx.rect(sizeX, 0, 1, 1)
 
     gfx.color(85, 85, 85)
-    gfx.rect(0, sizeY, sizeX, 2) --bottom shadow
-    gfx.rect(sizeX, 1, 2, sizeY - 1) --right shadow
-    gfx.rect(sizeX -1 , sizeY -1 , 2, 2) -- right bottom corner
-    gfx.rect(-1, sizeY, 1, 1) --left bottom corner
+    gfx.rect(0, sizeY, sizeX, 2)         --bottom shadow
+    gfx.rect(sizeX, 1, 2, sizeY - 1)     --right shadow
+    gfx.rect(sizeX - 1, sizeY - 1, 2, 2) -- right bottom corner
+    gfx.rect(-1, sizeY, 1, 1)            --left bottom corner
     gfx.text(5, 4, 'Inventory')
     gfx.text(sizeX - 7, 4, 'X')
 
@@ -79,7 +75,7 @@ function render(dt)
                 slotRender() --renders current slot
             end]]
             slot = slotloop --global slot #
-            slotRender() --renders current slot
+            slotRender()    --renders current slot
         end
     end
 end
