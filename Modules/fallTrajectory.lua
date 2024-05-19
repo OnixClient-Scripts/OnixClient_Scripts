79a45ff8ca06f6ad2f98819a0ba28cbf32f706af
WillDrac <101514340+O2Flash20@users.noreply.github.com>
2024-05-19 15:31:29 -0400

    changed target and fixed a couple of things

--- Modules/fallTrajectory.lua
+++ Modules/fallTrajectory.lua
@@ -3,16 +3,17 @@
 name = "Fall Trajectory"
 description = "Shows you where you'll fall"
 
-importLib("playerPhysics")
+importLib("PlayerPhysics")
 importLib("vectors")
 
 topCol = client.settings.addNamelessColor("Will land on block color", { 255, 255, 255, 255 })
 sideCol = client.settings.addNamelessColor("Will hit the side of block color", { 255, 0, 0, 255 })
+targetSize = client.settings.addNamelessFloat("Target size", 1, 6, 1.5)
 
 function render(dt)
     PlayerPhysics.update(dt)
 
-    if PlayerPhysics.velocity.y < -10 then --player is falling
+    if PlayerPhysics.velocity.y < -10 and not player.getFlag(32) then --player is falling and not with elytra
         local lastPosition
         for i = 1, 100, 1 do
             local hasACollision = false --keep track of whether or not a collision has been found yet
@@ -41,12 +42,12 @@ function render(dt)
                             hasACollision = true
                             local isTopFace = 0
                             if hit.blockFace == 1 then isTopFace = 1 end
-                            table.insert(blockCollisions, vec:new(hit.px, hit.py, hit.pz, isTopFace))
+                            table.insert(blockCollisions, vec:new(hit.px, hit.py, hit.pz, isTopFace, j, k))
                         end
                     end
                 end
 
-                local highestBlockCollision = vec:new(0, -1000, 0, 0) --just a placeholder for now
+                local highestBlockCollision = vec:new(0, -1000, 0, 0, 0, 0) --just a placeholder for now
                 for j = 1, #blockCollisions do
                     if blockCollisions[j].y > highestBlockCollision.y then
                         highestBlockCollision = blockCollisions[j]:copy()
@@ -55,17 +56,29 @@ function render(dt)
 
                 -- draw the target
                 local targetX, targetY = gfx.worldToScreen(
-                    highestBlockCollision.x,
+                    highestBlockCollision.x - highestBlockCollision.components[5],
                     highestBlockCollision.y,
-                    highestBlockCollision.z
+                    highestBlockCollision.z - highestBlockCollision.components[6]
                 )
-                if targetX and targetY then
+                if targetX and targetY and #blockCollisions > 0 then
                     if highestBlockCollision.w == 1 then
                         gfx.color(topCol.value.r, topCol.value.g, topCol.value.b, topCol.value.a)
                     else
                         gfx.color(sideCol.value.r, sideCol.value.g, sideCol.value.b, sideCol.value.a)
                     end
-                    gfx.text(targetX - 2.4, targetY - 4, "x")
+
+                    gfx.line(
+                        targetX - targetSize.value,
+                        targetY - targetSize.value,
+                        targetX + targetSize.value,
+                        targetY + targetSize.value
+                    )
+                    gfx.line(
+                        targetX - targetSize.value,
+                        targetY + targetSize.value,
+                        targetX + targetSize.value,
+                        targetY - targetSize.value
+                    )
                 end
             end
             if hasACollision then break end
