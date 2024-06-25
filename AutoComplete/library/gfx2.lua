64e8e2daf981c32410ce03027c80ba1db9cd33e8
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2024-06-24 21:24:33 -0400

    Update gfx2.lua

--- AutoComplete/library/gfx2.lua
+++ AutoComplete/library/gfx2.lua
@@ -394,6 +394,11 @@ function gfx2.cdrawTexture(x, y, width, height, texture, srcStartUvX, srcStartUv
 ---@param outputPath string The path to save the file to
 ---@param copyToClipboard boolean|nil If it should also copy the image to the clipboard (default false)
 ---@return boolean saved If the file was saved or not
+function gfx2.dumpTextureToFile(texture, outputPath, copyToClipboard) endcSizeUvX, srcSizeUvY, opacity) end
+
+---Dumps a texture from the game to a Gfx2Texture (more capable than extract texture)
+---@param texture string The texture to dump
+---@return Gfx2Texture|mil tex The gfx2 texture!
 function gfx2.dumpTexture(texture, outputPath, copyToClipboard) end
 
 
