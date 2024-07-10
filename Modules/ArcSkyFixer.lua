69136348e87daee754bbc62752acb91932e14ea2
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2024-07-10 16:24:34 -0400

    Update ArcSkyFixer.lua

--- Modules/ArcSkyFixer.lua
+++ Modules/ArcSkyFixer.lua
@@ -2,6 +2,6 @@ name="Intel Arc Sky Fixer"
 description="Fixes the sky being black every now and then on Intel Arc GPUs."
 
 function render() 
-  gfx.color(255,255,255,1)
+  gfx.color(0,0,0,1)
   gfx.rect(0,0,gui.width(),gui.height())
 end
