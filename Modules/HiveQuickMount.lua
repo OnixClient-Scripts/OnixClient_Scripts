45af9ce8ed454e6a712cb82f4b0b12b433aafebd
rikku2008 <r1kkuu2008@gmail.com>
2024-11-29 17:22:34 +0900

    add options

--- Modules/HiveQuickMount.lua
+++ Modules/HiveQuickMount.lua
@@ -1,5 +1,5 @@
 name = "Hive Quick Mount"
-description = "Ride mounts by using hub cosmetic item without opening modals"
+description = "Hive Quick Mount"
 
 mounts = {
   { 0,  "Custom Name" },
@@ -13,11 +13,17 @@ mounts = {
   { 8,  "Dragon" },
   { 9,  "Wooden Chair" },
   { 10, "Helicopter" },
+  { 11, "Snappy" },
+  { 12, "Couch" },
 }
+
+isModuleRequest = false;
 
 client.settings.addInfo("While this module is enabled, you can't open hub cosmetic menu.")
 mountId = client.settings.addNamelessEnum("Mount Name", 1, mounts)
 customMountName = client.settings.addNamelessTextbox("Mount Name", "")
+client.settings.addAir(1)
+sneakingDisable = client.settings.addNamelessBool("Disabled while sneaking", false)
 
 function getTableItemByNumber(table, index)
   for _, item in ipairs(table) do
@@ -51,14 +57,12 @@ function onModalRequested(arg1, arg2)
   ---@type ModalFormReplyer
   local response = arg2
 
-  if not server.ip():find("hive") or request.type ~= "form" then
+  if not server.ip():find("hive") or request.type ~= "form" or not isModuleRequest then
     return
   end
-
   if request.form.title:find("Hub Cosmetics") then
     response.form:reply(2)
   end
-
   if request.form.title:find("Mount Selector") then
     local mountName = getTableItemByNumber(mounts, mountId.value)
     if (mountId.value == 0) then
@@ -70,9 +74,25 @@ function onModalRequested(arg1, arg2)
       if (button.text:find(mountName)) then
         print("§c[!] §rQuick Mount is enabled!")
         response.form:reply(i)
+        isModuleRequest = false;
       end
     end
   end
 end
 
+function onMouseInput(button, down)
+  if button ~= 2 or not down then return end
+
+  local isSneaking = player.getFlag(1)
+
+  local holdItem = player.inventory().at(player.inventory().selected)
+  if not holdItem then return end
+  if not isSneaking and sneakingDisable.value and holdItem.name == "hivehub:pet_egg" then
+    isModuleRequest = true
+  elseif not sneakingDisable.value then
+    isModuleRequest = true
+  end
+end
+
 event.listen("ModalRequested", onModalRequested)
+event.listen("MouseInput", onMouseInput)
