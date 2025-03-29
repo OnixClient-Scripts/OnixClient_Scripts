505c5e046e1fd2cd2511529ae293e38af821336c
poophater11!1 <126386349+PoopyHaters@users.noreply.github.com>
2025-03-29 19:35:08 +0300

    TotemCounter
    TotemCounter

--- Modules/TotemCounter.lua
+++ Modules/TotemCounter.lua
@@ -1,5 +1,5 @@
 name = "Totem Counter"
-description = name
+description = "A simple TotemCounter script"
 
 --[[
     Totem counter script
@@ -13,21 +13,17 @@ sizeX = 12
 sizeY = 12
 
 totemPath = "textures/items/totem"
-gapPath = "textures/items/golden_apple"
 
 TextColor = { 255, 255, 255, 255 }
-texts = client.settings.addNamelessFloat("Text Size", 0.1, 10, 0.3)
-
-
-function update(deltaTime)
-
-end
-
+texts = client.settings.addNamelessFloat("Text Size", 0.1, 10, 0.50)
+textc = client.settings.addNamelessColor("Text color", { 255, 255, 255, 255 })
+bgc = client.settings.addNamelessColor("Background color", { 0, 0, 0, 127 })
 function render()
     local inventory = player.inventory()
     local offhand = player.inventory().offhand()
     local totemCount = 0
     local totemId = "totem_of_undying"
+
     local selected = inventory.at(inventory.selected)
     for i = 1, inventory.size do
         local slot = inventory.at(i)
@@ -35,14 +31,13 @@ function render()
             if (slot.name == totemId) then
                 totemCount = totemCount + slot.count
                 itemLocation = slot.location
-            if offhand == "totem_of_undying" then
-                totemCount = totemCount + 1
-            end
         end
     end
 end
     if (itemLocation ~= "") then
-        gui.font()
+        gfx.color(bgc)
+        gfx.rect(0, 0, 12, 12)
+        gfx.color(textc)
         gfx.texture(0, 0, 12, 12, totemPath)
         gfx.text(5, 6, totemCount, texts.value)
     end
